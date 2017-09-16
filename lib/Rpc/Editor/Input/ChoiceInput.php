83a96adaeca40257f69253aa06b183a334728a0f
dantleech <dan.t.leech@gmail.com>
2017-09-16 13:39:47 +0200

    Class Move RPC

---
+++ lib/Rpc/Editor/Input/ChoiceInput.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Phpactor\Rpc\Editor\Input;
+
+class ChoiceInput implements Input
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $label;
+
+    /**
+     * @var string
+     */
+    private $default;
+
+    /**
+     * @var array
+     */
+    private $choices;
+
+    private function __construct(string $name, string $label, array $choices, string $default)
+    {
+        $this->name = $name;
+        $this->label = $label;
+        $this->default = $default;
+        $this->choices = $choices;
+    }
+
+    public static function fromNameLabelChoicesAndDefault(string $name, string $label, array $choices, string $default = null)
+    {
+        return new self($name, $label, $choices, $default);
+    }
+
+    public function type(): string
+    {
+        return 'choice';
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function label(): string
+    {
+        return $this->label;
+    }
+
+    public function parameters(): array
+    {
+        return [
+            'default' => $this->default,
+            'label' => $this->label,
+            'choices' => $this->choices,
+        ];
+    }
+}
+
