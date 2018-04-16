6dff30ed40ee0fb80de05950c73a8761011303be
dantleech <dan.t.leech@gmail.com>
2018-04-16 19:40:42 +0200

    [RPC] Request validation (no more undefined index errors) (#417)

--- lib/Extension/Rpc/Request.php
+++ lib/Extension/Rpc/Request.php
@@ -2,6 +2,8 @@
 
 namespace Phpactor\Extension\Rpc;
 
+use InvalidArgumentException;
+
 class Request
 {
     const KEY_ACTION = 'action';
@@ -28,15 +30,25 @@ class Request
         return new self($name, $parameters);
     }
 
-    public static function fromArray(array $actionConfig)
+    public static function fromArray(array $actionConfig): Request
     {
+        if (!isset($actionConfig[self::KEY_ACTION])) {
+            throw new InvalidArgumentException(
+                'Missing "action" key'
+            );
+        }
+
         $validKeys = [ self::KEY_ACTION, self::KEY_PARAMETERS ];
         if ($diff = array_diff(array_keys($actionConfig), $validKeys)) {
-            throw new \InvalidArgumentException(sprintf(
+            throw new InvalidArgumentException(sprintf(
                 'Invalid request keys "%s", valid keys: "%s"',
                 implode('", "', $diff),
                 implode('", "', $validKeys)
             ));
+        }
+
+        if (!isset($actionConfig[self::KEY_PARAMETERS])) {
+            $actionConfig[self::KEY_PARAMETERS] = [];
         }
 
         return new self($actionConfig[self::KEY_ACTION], $actionConfig[self::KEY_PARAMETERS]);
