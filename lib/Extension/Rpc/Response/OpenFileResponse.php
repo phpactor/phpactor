c91448466821ece0e2cae527539cfa3f5cdbfe5b
Daniel Leech <dan.t.leech@gmail.com>
2018-09-01 10:06:12 +0100

    fix regression: force reload file before apply changes for rename op.

--- lib/Extension/Rpc/Response/OpenFileResponse.php
+++ lib/Extension/Rpc/Response/OpenFileResponse.php
@@ -16,13 +16,19 @@ class OpenFileResponse implements Response
      */
     private $offset;
 
-    private function __construct(string $path, int $offset = 0)
+    /**
+     * @var bool
+     */
+    private $forceReload;
+
+    private function __construct(string $path, int $offset = 0, bool $forceReload = false)
     {
         $this->path = $path;
         $this->offset = $offset;
+        $this->forceReload = $forceReload;
     }
 
-    public static function fromPath(string $path)
+    public static function fromPath(string $path): OpenFileResponse
     {
         return new self($path);
     }
@@ -42,11 +48,20 @@ class OpenFileResponse implements Response
         return [
             'path' => $this->path,
             'offset' => $this->offset,
+            'force_reload' => $this->forceReload
         ];
     }
 
     public function path(): string
     {
         return $this->path;
+    }
+
+    public function withForcedReload(bool $bool)
+    {
+        $new = clone $this;
+        $new->forceReload = $bool;
+
+        return $new;
     }
 }
