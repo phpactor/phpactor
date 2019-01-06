d4c43598829385b4005417569bd3e40410997187
Daniel Leech <dan.t.leech@gmail.com>
2019-01-06 18:46:48 +0000

    use composer to get deps to test

--- .travis/build-dashboard.php
+++ .travis/build-dashboard.php
@@ -1,26 +1,40 @@
 <?php
 
-$composer = json_decode(file_get_contents(__DIR__ . '/../composer.json'), true);
+$packages = json_decode(`composer show --format=json`, true);
 
-$packages = array_filter($composer['require'], function ($key) {
-    return 0 === strpos($key, 'phpactor');
-}, ARRAY_FILTER_USE_KEY);
-
-ksort($packages);
+$packages = array_filter($packages['installed'], function ($package) {
+    return 0 === strpos($package['name'], 'phpactor');
+});
 
 $output = [];
 $output[] = 'Travis Dashboard';
 $output[] = '================';
 $output[] = '';
-$output[] = 'Dependencies';
-$output[] = '------------';
+$output[] = '| Package | Travis | Packagist |';
+$output[] = '| ------- | ------ | --------- |';
 
-$output[] = '| Package | Version | Badge |';
-$output[] = '| ------- | ------- | ----- |';
-render_package_table($output, $packages);
+foreach ($packages as $package) {
+    $output[] = sprintf(
+        '| %s | %s | %s',
+        sprintf(
+            '<a href="https://github.com/%s">%s</a>',
+            $package['name'],
+            $package['name']
+        ),
+        sprintf(
+            '[![master](https://travis-ci.org/%s.svg?branch=master)](https://travis-ci.org/%s)',
+            $package['name'],
+            $package['name']
+        ),
+        sprintf(
+            '[![Latest stable](https://img.shields.io/packagist/v/%s.svg)](https://packagist.org/packages/%s)',
+            $package['name'],
+            $package['name']
+        )
+    );
+}
 
-$extensions = array_filter(explode(PHP_EOL, file_get_contents(__DIR__ . '/extensions-to-test')));
-
+$extensions = explode(PHP_EOL, file_get_contents(__DIR__ . '/extensions-to-test'));
 $output[] = '';
 $output[] = 'Extensions';
 $output[] = '----------';
--- .travis/list-deps.php
+++ .travis/list-deps.php
@@ -1,11 +1,14 @@
 <?php
 
-$composer = json_decode(file_get_contents(__DIR__ . '/../composer.json'), true);
+$packages = json_decode(`composer show --format=json`, true);
 
-$packages = array_filter($composer['require'], function ($key) {
-    return 0 === strpos($key, 'phpactor');
-}, ARRAY_FILTER_USE_KEY);
+$packages = array_filter($packages['installed'], function ($package) {
+    return 0 === strpos($package['name'], 'phpactor');
+});
+$packages = array_map(function ($package) {
+    return $package['name'];
+}, $packages);
 
-foreach ($packages as $package => $version) {
+foreach ($packages as $package) {
     echo $package . PHP_EOL;
 }
