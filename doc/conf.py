58cf127717ee1a1b528083ca65a2d4b54e2d5ae6
Daniel Leech <dan.t.leech@gmail.com>
2020-05-02 16:26:33 +0100

    Removed unnecessaries

--- doc/conf.py
+++ doc/conf.py
@@ -75,4 +75,3 @@ html_sidebars = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-sphinxemoji_style = 'twemoji'
