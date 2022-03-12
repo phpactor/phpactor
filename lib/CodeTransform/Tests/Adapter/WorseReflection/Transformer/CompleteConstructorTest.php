8dceed2b8347e212b391aab33af9957b57709fd0
Daniel Leech <dan.t.leech@gmail.com>
2022-03-12 10:11:38 +0100

    Fix tests

--- lib/CodeTransform/Tests/Adapter/WorseReflection/Transformer/CompleteConstructorTest.php
+++ lib/CodeTransform/Tests/Adapter/WorseReflection/Transformer/CompleteConstructorTest.php
@@ -15,7 +15,7 @@ class CompleteConstructorTest extends WorseTestCase
     public function testDiagnostics(string $example, int $expectedCount): void
     {
         $source = SourceCode::fromString($example);
-        $transformer = new CompleteConstructor($this->reflectorForWorkspace($example), $this->updater());
+        $transformer = new CompleteConstructor($this->reflectorForWorkspace($example), $this->updater(), 'private');
         $this->assertCount($expectedCount, $transformer->diagnostics($source));
     }
 
@@ -50,7 +50,7 @@ class CompleteConstructorTest extends WorseTestCase
     public function testCompleteConstructor(string $example, string $expected): void
     {
         $source = SourceCode::fromString($example);
-        $transformer = new CompleteConstructor($this->reflectorForWorkspace($example), $this->updater());
+        $transformer = new CompleteConstructor($this->reflectorForWorkspace($example), $this->updater(), 'private');
         $transformed = $transformer->transform($source);
         $this->assertEquals((string) $expected, (string) $transformed->apply($source));
     }
