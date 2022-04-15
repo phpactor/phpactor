fc43db555f71c36324571456a0e0f989cbed46ac
Daniel Leech <dan.t.leech@gmail.com>
2022-04-15 10:06:33 +0200

    Remove dead code

--- lib/WorseReflection/Core/Inference/TypeCombinator.php
+++ lib/WorseReflection/Core/Inference/TypeCombinator.php
@@ -3,7 +3,6 @@
 namespace Phpactor\WorseReflection\Core\Inference;
 
 use Phpactor\WorseReflection\Core\Type;
-use Phpactor\WorseReflection\Core\Type\NotType;
 use Phpactor\WorseReflection\Core\Type\UnionType;
 
 class TypeCombinator
@@ -19,29 +18,6 @@ class TypeCombinator
     {
         $originalType = UnionType::toUnion($originalType);
         return $originalType->narrowTo($type);
-    }
-
-    public static function add(Type $originalType, Type $type): Type
-    {
-        $originalType = UnionType::toUnion($originalType);
-        $type = UnionType::toUnion($type);
-
-        return $originalType->merge($type);
-    }
-
-    public static function applyType(Type $originalType, Type $type): Type
-    {
-        if ($type instanceof NotType) {
-
-            // double negative - this should probably be smarter
-            if ($type->type instanceof NotType) {
-                return self::applyType($originalType, $type->type->type);
-            }
-
-            return self::remove($originalType, $type->type);
-        }
-
-        return self::narrowTo($originalType, $type);
     }
 
     public static function subtract(Type $type, Type $from): Type
