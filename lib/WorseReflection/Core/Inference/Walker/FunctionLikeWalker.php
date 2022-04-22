8d42ad6937c3e0472916decf6d13f25bee3b7612
Daniel Leech <dan.t.leech@gmail.com>
2022-04-23 00:29:32 +0200

    Fix CS

--- lib/WorseReflection/Core/Inference/Walker/FunctionLikeWalker.php
+++ lib/WorseReflection/Core/Inference/Walker/FunctionLikeWalker.php
@@ -5,7 +5,6 @@ namespace Phpactor\WorseReflection\Core\Inference\Walker;
 use Microsoft\PhpParser\MissingToken;
 use Microsoft\PhpParser\Node;
 use Microsoft\PhpParser\Node\Expression\ArrowFunctionCreationExpression;
-use PhpParser\Node\Expr\ArrowFunction;
 use Phpactor\WorseReflection\Core\Inference\FrameResolver;
 use Phpactor\WorseReflection\Core\Inference\Frame;
 use Microsoft\PhpParser\FunctionLike;
