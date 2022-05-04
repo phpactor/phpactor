4938e06ce5515126742e84655d81667cf09b4fe8
Daniel Leech <dan.t.leech@gmail.com>
2022-05-04 07:23:51 +0200

    Fix in array behavior

--- lib/WorseReflection/Core/Inference/FunctionStub/InArrayStub.php
+++ lib/WorseReflection/Core/Inference/FunctionStub/InArrayStub.php
@@ -7,6 +7,7 @@ use Phpactor\WorseReflection\Core\Inference\FunctionStub;
 use Phpactor\WorseReflection\Core\Inference\NodeContext;
 use Phpactor\WorseReflection\Core\Inference\Symbol;
 use Phpactor\WorseReflection\Core\Inference\TypeAssertion;
+use Phpactor\WorseReflection\Core\Inference\TypeCombinator;
 use Phpactor\WorseReflection\Core\Type;
 use Phpactor\WorseReflection\Core\TypeFactory;
 use Phpactor\WorseReflection\Core\Type\ArrayLiteral;
@@ -28,13 +29,19 @@ class InArrayStub implements FunctionStub
             return $context;
         }
 
+        $union = TypeFactory::union(...$arrayType->iterableValueTypes());
         return $context->withTypeAssertion(
             TypeAssertion::forContext(
                 $args->at(0),
-                function (Type $type) use ($arrayType) {
-                    return TypeFactory::union(...$arrayType->iterableValueTypes());
+                function (Type $type) use ($union) {
+                    return $union;
                 },
-                function (Type $type) {
+                function (Type $type) use ($union) {
+                    $type = TypeCombinator::subtract($union, $type);
+                    if (!$type->isDefined()) {
+                        $type = $union->generalize();
+                    }
+
                     return $type;
                 }
             )
