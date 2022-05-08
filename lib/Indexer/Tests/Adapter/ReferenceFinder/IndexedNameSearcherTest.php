a53d2d2ad1d552b4da6003b44a97071ee437ea13
Daniel Leech <dan.t.leech@gmail.com>
2022-05-08 12:34:58 +0200

    More test case

--- lib/Indexer/Tests/Adapter/ReferenceFinder/IndexedNameSearcherTest.php
+++ lib/Indexer/Tests/Adapter/ReferenceFinder/IndexedNameSearcherTest.php
@@ -39,4 +39,38 @@ class IndexedNameSearcherTest extends IndexTestCase
 
         $this->fail('Could not find interface');
     }
+
+    public function testSearcherForEnum(): void
+    {
+        $this->workspace()->put('project/Foobar.php', '<?php enum Foobar {}');
+        $agent = $this->indexAgent();
+        $agent->indexer()->getJob()->run();
+        $searcher = new IndexedNameSearcher($agent->search());
+
+        foreach ($searcher->search('Foo', NameSearcherType::ENUM) as $result) {
+            assert($result instanceof NameSearchResult);
+            self::assertEquals('Foobar', $result->name()->head()->__toString());
+            self::assertStringContainsString('Foobar.php', $result->uri()->__toString());
+            return;
+        }
+
+        $this->fail('Could not find interface');
+    }
+
+    public function testSearcherForTrait(): void
+    {
+        $this->workspace()->put('project/Foobar.php', '<?php trait Foobar {}');
+        $agent = $this->indexAgent();
+        $agent->indexer()->getJob()->run();
+        $searcher = new IndexedNameSearcher($agent->search());
+
+        foreach ($searcher->search('Foo', NameSearcherType::TRAIT) as $result) {
+            assert($result instanceof NameSearchResult);
+            self::assertEquals('Foobar', $result->name()->head()->__toString());
+            self::assertStringContainsString('Foobar.php', $result->uri()->__toString());
+            return;
+        }
+
+        $this->fail('Could not find interface');
+    }
 }
