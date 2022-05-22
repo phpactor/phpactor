c34e2892dfd8fbec2bce73815498d783b7c41cb6
Camille Dejoye <camilledejoye@gmail.com>
2022-05-22 11:56:25 +0200

    chore: refactor ObjectBehaviorMemberProvider

--- lib/Extension/PhpSpec/Provider/ObjectBehaviorMemberProvider.php
+++ lib/Extension/PhpSpec/Provider/ObjectBehaviorMemberProvider.php
@@ -5,12 +5,15 @@ namespace Phpactor\Extension\PhpSpec\Provider;
 use Phpactor\WorseReflection\Core\ClassName;
 use Phpactor\WorseReflection\Core\Exception\NotFound;
 use Phpactor\WorseReflection\Core\Reflection\Collection\ReflectionMemberCollection;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionClass;
 use Phpactor\WorseReflection\Core\Reflection\ReflectionClassLike;
 use Phpactor\WorseReflection\Core\ServiceLocator;
 use Phpactor\WorseReflection\Core\TypeFactory;
+use Phpactor\WorseReflection\Core\Type\ReflectedClassType;
 use Phpactor\WorseReflection\Core\Virtual\Collection\VirtualReflectionMemberCollection;
 use Phpactor\WorseReflection\Core\Virtual\Collection\VirtualReflectionMethodCollection;
 use Phpactor\WorseReflection\Core\Virtual\ReflectionMemberProvider;
+use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionMember;
 use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionMethod;
 use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionProperty;
 
@@ -28,26 +31,26 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
 
     public function provideMembers(
         ServiceLocator $serviceLocator,
-        ReflectionClassLike $class
+        ReflectionClassLike $specClass
     ): ReflectionMemberCollection {
-        $subjectClassName = explode('\\', $class->name()->namespace());
+        $specClassName = explode('\\', $specClass->name()->namespace());
 
-        if (false === $this->isSpecCandidate($class, $subjectClassName)) {
+        if (false === $this->isSpecCandidate($specClass, $specClassName)) {
             return VirtualReflectionMemberCollection::fromMembers([]);
         }
 
-        array_shift($subjectClassName);
-        $subjectClassName[] = substr($class->name()->short(), 0, -4);
-        $subjectClassName = implode('\\', $subjectClassName);
+        array_shift($specClassName);
+        $specClassName[] = substr($specClass->name()->short(), 0, -4);
+        $specClassName = implode('\\', $specClassName);
 
         try {
-            $subjectClass = $serviceLocator->reflector()->reflectClass($subjectClassName);
+            $testedClass = $serviceLocator->reflector()->reflectClass($specClassName);
         } catch (NotFound $e) {
             $serviceLocator->logger()->warning(sprintf(
                 'Phpspec extension could not locate inferred class name "%s" '.
                 'for spec class "%s": %s',
-                $class->name()->full(),
-                $subjectClassName,
+                $specClass->name()->full(),
+                $specClassName,
                 $e->getMessage()
             ));
             return VirtualReflectionMethodCollection::fromMembers([]);
@@ -55,55 +58,72 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
 
         $subjectType = TypeFactory::reflectedClass($serviceLocator->reflector(), self::SUBJECT_CLASS);
 
-        $virtualMethods = [];
-        foreach ($subjectClass->methods() as $subjectMethod) {
-            if (false === $subjectMethod->visibility()->isPublic()) {
-                continue;
-            }
-
-            $method = VirtualReflectionMethod::fromReflectionMethod($subjectMethod);
-            $method = $method
-                ->withInferredType($method->inferredType()->addType($subjectType))
-                ->withType($subjectType)
-            ;
-            $virtualMethods[] = $method;
-        }
-
-        $virtualProperties = [];
-        foreach ($subjectClass->properties() as $subjectProperty) {
-            if (false === $subjectProperty->visibility()->isPublic()) {
-                continue;
-            }
-
-            $property = VirtualReflectionProperty::fromReflectionProperty($subjectProperty);
-            $property = $property
-                ->withInferredType($property->inferredType()->addType($subjectType))
-                ->withType($subjectType)
-            ;
-            $virtualProperties[] = $property;
-        }
-
-        return VirtualReflectionMemberCollection::fromMembers(array_merge($virtualMethods, $virtualProperties));
+        return VirtualReflectionMemberCollection::fromMembers(array_merge(
+            $this->retrievePublicMethods($testedClass, $subjectType),
+            $this->retrievePublicProperties($testedClass, $subjectType),
+        ));
     }
 
     /**
-     * @param array<string> $subjectClassName
+     * @param array<string> $specClassName
      */
-    private function isSpecCandidate(ReflectionClassLike $class, array $subjectClassName): bool
+    private function isSpecCandidate(ReflectionClassLike $specClass, array $specClassName): bool
     {
-        if (!$class->isInstanceOf(ClassName::fromString(self::OBJECT_BEHAVIOR_CLASS))) {
+        if (!$specClass->isInstanceOf(ClassName::fromString(self::OBJECT_BEHAVIOR_CLASS))) {
             return false;
         }
         
-        if (array_shift($subjectClassName) !== $this->specPrefix) {
+        if (array_shift($specClassName) !== $this->specPrefix) {
             return false;
         }
         
-        $suffix = substr($class->name()->short(), -4);
+        $suffix = substr($specClass->name()->short(), -4);
         if ('Spec' !== $suffix) {
             return false;
         }
 
         return true;
+    }
+
+    /**
+     * @return array<VirtualReflectionMember>
+     */
+    private function retrievePublicMethods(ReflectionClass $testedClass, ReflectedClassType $subjectType): array
+    {
+        $virtualMethods = [];
+        foreach ($testedClass->methods() as $method) {
+            if (false === $method->visibility()->isPublic()) {
+                continue;
+            }
+        
+            $virtualMethod = VirtualReflectionMethod::fromReflectionMethod($method);
+            $virtualMethods[] = $virtualMethod
+                ->withInferredType($virtualMethod->inferredType()->addType($subjectType))
+                ->withType($subjectType)
+            ;
+        }
+
+        return $virtualMethods;
+    }
+
+    /**
+     * @return array<VirtualReflectionMember>
+     */
+    private function retrievePublicProperties(ReflectionClass $testedClass, ReflectedClassType $subjectType): array
+    {
+        $virtualProperties = [];
+        foreach ($testedClass->properties() as $property) {
+            if (false === $property->visibility()->isPublic()) {
+                continue;
+            }
+        
+            $virtualProperty = VirtualReflectionProperty::fromReflectionProperty($property);
+            $virtualProperties[] = $virtualProperty
+                ->withInferredType($virtualProperty->inferredType()->addType($subjectType))
+                ->withType($subjectType)
+            ;
+        }
+
+        return $virtualProperties;
     }
 }
