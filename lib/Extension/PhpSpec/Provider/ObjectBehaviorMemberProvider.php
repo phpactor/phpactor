9d8b95af237c49422c9d3482e274cdc8aaf23e78
Camille Dejoye <camilledejoye@gmail.com>
2022-05-24 10:53:17 +0200

    fix: use new member collection type

--- lib/Extension/PhpSpec/Provider/ObjectBehaviorMemberProvider.php
+++ lib/Extension/PhpSpec/Provider/ObjectBehaviorMemberProvider.php
@@ -4,16 +4,15 @@ namespace Phpactor\Extension\PhpSpec\Provider;
 
 use Phpactor\WorseReflection\Core\ClassName;
 use Phpactor\WorseReflection\Core\Exception\NotFound;
+use Phpactor\WorseReflection\Core\Reflection\Collection\HomogeneousReflectionMemberCollection;
 use Phpactor\WorseReflection\Core\Reflection\Collection\ReflectionMemberCollection;
 use Phpactor\WorseReflection\Core\Reflection\ReflectionClass;
 use Phpactor\WorseReflection\Core\Reflection\ReflectionClassLike;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionMember;
 use Phpactor\WorseReflection\Core\ServiceLocator;
 use Phpactor\WorseReflection\Core\TypeFactory;
 use Phpactor\WorseReflection\Core\Type\ReflectedClassType;
-use Phpactor\WorseReflection\Core\Virtual\Collection\VirtualReflectionMemberCollection;
-use Phpactor\WorseReflection\Core\Virtual\Collection\VirtualReflectionMethodCollection;
 use Phpactor\WorseReflection\Core\Virtual\ReflectionMemberProvider;
-use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionMember;
 use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionMethod;
 use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionProperty;
 
@@ -21,7 +20,7 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
 {
     private const OBJECT_BEHAVIOR_CLASS = '\PhpSpec\ObjectBehavior';
     private const SUBJECT_CLASS = '\PhpSpec\Wrapper\Subject';
-    
+
     private string $specPrefix;
 
     public function __construct(string $specPrefix = 'spec')
@@ -29,6 +28,9 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
         $this->specPrefix = $specPrefix;
     }
 
+    /**
+     * @return ReflectionMemberCollection<ReflectionMember>
+     */
     public function provideMembers(
         ServiceLocator $serviceLocator,
         ReflectionClassLike $specClass
@@ -36,7 +38,7 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
         $specClassName = explode('\\', $specClass->name()->namespace());
 
         if (false === $this->isSpecCandidate($specClass, $specClassName)) {
-            return VirtualReflectionMemberCollection::fromMembers([]);
+            return HomogeneousReflectionMemberCollection::fromMembers([]);
         }
 
         array_shift($specClassName);
@@ -47,18 +49,18 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
             $testedClass = $serviceLocator->reflector()->reflectClass($specClassName);
         } catch (NotFound $e) {
             $serviceLocator->logger()->warning(sprintf(
-                'Phpspec extension could not locate inferred class name "%s" '.
-                'for spec class "%s": %s',
+                'Phpspec extension could not locate inferred class name "%s" ' .
+                    'for spec class "%s": %s',
                 $specClass->name()->full(),
                 $specClassName,
                 $e->getMessage()
             ));
-            return VirtualReflectionMethodCollection::fromMembers([]);
+            return HomogeneousReflectionMemberCollection::fromMembers([]);
         }
 
         $subjectType = TypeFactory::reflectedClass($serviceLocator->reflector(), self::SUBJECT_CLASS);
 
-        return VirtualReflectionMemberCollection::fromMembers(array_merge(
+        return HomogeneousReflectionMemberCollection::fromMembers(array_merge(
             $this->retrievePublicMethods($testedClass, $subjectType),
             $this->retrievePublicProperties($testedClass, $subjectType),
         ));
@@ -72,11 +74,11 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
         if (!$specClass->isInstanceOf(ClassName::fromString(self::OBJECT_BEHAVIOR_CLASS))) {
             return false;
         }
-        
+
         if (array_shift($specClassName) !== $this->specPrefix) {
             return false;
         }
-        
+
         $suffix = substr($specClass->name()->short(), -4);
         if ('Spec' !== $suffix) {
             return false;
@@ -86,7 +88,7 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
     }
 
     /**
-     * @return array<VirtualReflectionMember>
+     * @return array<VirtualReflectionMethod>
      */
     private function retrievePublicMethods(ReflectionClass $testedClass, ReflectedClassType $subjectType): array
     {
@@ -95,19 +97,18 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
             if (false === $method->visibility()->isPublic()) {
                 continue;
             }
-        
+
             $virtualMethod = VirtualReflectionMethod::fromReflectionMethod($method);
             $virtualMethods[] = $virtualMethod
                 ->withInferredType(TypeFactory::intersection($virtualMethod->inferredType(), $subjectType))
-                ->withType($subjectType)
-            ;
+                ->withType($subjectType);
         }
 
         return $virtualMethods;
     }
 
     /**
-     * @return array<VirtualReflectionMember>
+     * @return array<VirtualReflectionProperty>
      */
     private function retrievePublicProperties(ReflectionClass $testedClass, ReflectedClassType $subjectType): array
     {
@@ -116,12 +117,11 @@ class ObjectBehaviorMemberProvider implements ReflectionMemberProvider
             if (false === $property->visibility()->isPublic()) {
                 continue;
             }
-        
+
             $virtualProperty = VirtualReflectionProperty::fromReflectionProperty($property);
             $virtualProperties[] = $virtualProperty
                 ->withInferredType(TypeFactory::intersection($virtualProperty->inferredType(), $subjectType))
-                ->withType($subjectType)
-            ;
+                ->withType($subjectType);
         }
 
         return $virtualProperties;
