38ba41d9a6b3842770e0c35ce21482756799cfec
mamazu <14860264+mamazu@users.noreply.github.com>
2022-05-27 10:47:38 +0200

    Adding configuration reference for the Console extension (#1662)
    * Adding configuration reference for the Console extension
    
    * Using the naming convention

--- lib/Extension/Console/ConsoleExtension.php
+++ lib/Extension/Console/ConsoleExtension.php
@@ -13,12 +13,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ConsoleExtension implements Extension
 {
-    const TAG_COMMAND = 'console.command';
-    const SERVICE_COMMAND_LOADER = 'console.command_loader';
-    const SERVICE_OUTPUT = 'console.output';
-    const SERVICE_INPUT = 'console.input';
+    public const TAG_COMMAND = 'console.command';
+    public const SERVICE_COMMAND_LOADER = 'console.command_loader';
+    public const SERVICE_OUTPUT = 'console.output';
+    public const SERVICE_INPUT = 'console.input';
+    private const PARAM_VERBOSITY = 'console.verbosity';
+    private const PARAM_DECORATED = 'console.decorated';
 
-    
+
     public function load(ContainerBuilder $container): void
     {
         $container->register(self::SERVICE_COMMAND_LOADER, function (Container $container) {
@@ -39,8 +41,8 @@ class ConsoleExtension implements Extension
 
         $container->register(self::SERVICE_OUTPUT, function (Container $container) {
             return new ConsoleOutput(
-                $container->getParameter('console.verbosity'),
-                $container->getParameter('console.decorated')
+                $container->getParameter(self::PARAM_VERBOSITY),
+                $container->getParameter(self::PARAM_DECORATED)
             );
         });
 
@@ -49,12 +51,30 @@ class ConsoleExtension implements Extension
         });
     }
 
-    
+
     public function configure(Resolver $schema): void
     {
         $schema->setDefaults([
-            'console.verbosity' => OutputInterface::VERBOSITY_NORMAL,
-            'console.decorated' => null,
+            self::PARAM_VERBOSITY => OutputInterface::VERBOSITY_NORMAL,
+            self::PARAM_DECORATED => null,
+        ]);
+        $schema->setDescriptions([
+            self::PARAM_VERBOSITY => 'Verbosity level',
+            self::PARAM_DECORATED => 'Whether to decorate messages (null for auto-guessing)',
+        ]);
+        $schema->setEnums([
+            self::PARAM_VERBOSITY => [
+                OutputInterface::VERBOSITY_QUIET,
+                OutputInterface::VERBOSITY_NORMAL,
+                OutputInterface::VERBOSITY_VERBOSE,
+                OutputInterface::VERBOSITY_VERY_VERBOSE,
+                OutputInterface::VERBOSITY_DEBUG,
+            ],
+            self::PARAM_DECORATED => [
+                true,
+                false,
+                null
+            ]
         ]);
     }
 }
