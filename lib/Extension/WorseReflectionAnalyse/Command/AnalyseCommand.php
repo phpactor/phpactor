990a8927faae105571e50cc3e7419e0ed0b26e7c
Daniel Leech <dan.t.leech@gmail.com>
2022-06-08 00:13:36 +0200

    ADd progress to diagnostics

--- lib/Extension/WorseReflectionAnalyse/Command/AnalyseCommand.php
+++ lib/Extension/WorseReflectionAnalyse/Command/AnalyseCommand.php
@@ -5,6 +5,7 @@ namespace Phpactor\Extension\WorseReflectionAnalyse\Command;
 use Phpactor\Extension\WorseReflectionAnalyse\Model\Analyser;
 use Phpactor\WorseReflection\Core\Diagnostics;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -29,28 +30,42 @@ class AnalyseCommand extends Command
         $this->addArgument(self::ARG_PATH, InputArgument::REQUIRED, 'Path to analyse');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $start = microtime(true);
+
         /**
          * @var array<string,Diagnostics> $results
          */
         $results = [];
-        foreach ($this->analyser->analyse($input->getArgument(self::ARG_PATH)) as $file => $diagnostics) {
+        $path = $input->getArgument(self::ARG_PATH);
+
+        $count = count(iterator_to_array($this->analyser->fileList($path), true));
+        $output->writeln('Analysing files...');
+        $output->writeln('');
+        $progress = new ProgressBar($output, $count);
+        $progress->start();
+
+        foreach ($this->analyser->analyse($path) as $file => $diagnostics) {
+            $progress->advance();
             $results[$file] = $diagnostics;
         }
+        $progress->finish();
+        $output->writeln('');
+        $output->writeln('');
 
-        $hasErrors = false;
+        $errorCount = 0;
 
         foreach ($results as $file => $diagnostics) {
             if (!count($diagnostics)) {
                 continue;
             }
-            $hasErrors = true;
             $output->writeln($file);
             $table = new Table($output);
             $table->setHeaders(['position', 'severity', 'message']);
             $table->setColumnMaxWidth(2, 60);
             foreach ($diagnostics as $diagnostic) {
+                $errorCount++;
                 $table->addRow([
                     sprintf('%s:%s', $diagnostic->range()->start()->toInt(), $diagnostic->range()->end()->toInt()),
                     $diagnostic->severity()->toString(),
@@ -60,8 +75,8 @@ class AnalyseCommand extends Command
             $table->render();
             $output->writeln('');
         }
+        $output->writeln(sprintf('%s problems in %s seconds', $errorCount, number_format(microtime(true) - $start, 4)));
 
-
-        return $hasErrors ? 1 : 0;
+        return $errorCount ? 1 : 0;
     }
 }
