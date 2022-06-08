3bd08123709fdc9ddaac1e27f2dcec415b345fa1
Daniel Leech <dan.t.leech@gmail.com>
2022-06-08 21:41:04 +0200

    Benchmark for method chain

--- lib/WorseReflection/Tests/Benchmarks/DiagnosticsBench.php
+++ lib/WorseReflection/Tests/Benchmarks/DiagnosticsBench.php
@@ -2,9 +2,13 @@
 
 namespace Phpactor\WorseReflection\Tests\Benchmarks;
 
+use Generator;
+use GlobIterator;
 use Phpactor\WorseReflection\Bridge\TolerantParser\Diagnostics\MissingMethodProvider;
 use Phpactor\WorseReflection\Reflector;
 use Phpactor\WorseReflection\ReflectorBuilder;
+use Phpactor\WorseReflection\Tests\Inference\TestAssertWalker;
+use SplFileInfo;
 
 /**
  * @Iterations(5)
@@ -18,18 +22,31 @@ class DiagnosticsBench
     {
         $this->reflector = ReflectorBuilder::create()
             ->addDiagnosticProvider(new MissingMethodProvider())
-            ->enableContextualSourceLocation()
-            ->enableCache()
             ->build();
     }
 
     /**
      * @BeforeMethods({"init"})
+     * @ParamProviders({"providePaths"})
+     * @param array{path:string} $params
      */
-    public function benchDiagnostics(): void
+    public function benchDiagnostics(array $params): void
     {
         $diagnostics = $this->reflector->diagnostics(
-            file_get_contents(__DIR__ . '/fixtures/diagnostics/lots_of_missing_methods.test')
+            (string)file_get_contents($params['path'])
         );
+    }
+
+    /**
+     * @return Generator<array{path:string}>
+     */
+    public function providePaths(): Generator
+    {
+        foreach ((new GlobIterator(__DIR__ . '/fixtures/diagnostics/*.test')) as $info) {
+            assert($info instanceof SplFileInfo);
+            yield $info->getFilename() => [
+                'path' => $info->getRealPath()
+            ];
+        }
     }
 }
---
+++ lib/WorseReflection/Tests/Benchmarks/fixtures/diagnostics/method_chain.test
@@ -0,0 +1,6 @@
+<?php
+
+class Fizz { public function bang(): Fizz {} }
+
+$fizz = new Fizz();
+$fizz->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang(->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang(->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang(->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang(->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang(->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang(->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang(->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang()->bang(->bang()->bang()->bang(->bang()->bang();
