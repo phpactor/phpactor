ed97d2ece96f4f734bc9c7523c46e8e6fe4324b7
przepompownia <przepompownia@users.noreply.github.com>
2022-06-15 00:03:14 +0200

    Highlight name only

--- lib/Extension/LanguageServerReferenceFinder/Model/Highlighter.php
+++ lib/Extension/LanguageServerReferenceFinder/Model/Highlighter.php
@@ -132,10 +132,18 @@ class Highlighter
     private function properties(Node $rootNode, string $name): Generator
     {
         foreach ($rootNode->getDescendantNodes() as $node) {
-            if ((
-                $node instanceof Parameter && null !== $node->visibilityToken
-                || $node instanceof Variable && $node->getFirstAncestor(PropertyDeclaration::class)
-            ) && (string)$node->getName() === $name) {
+            if ($node instanceof Parameter && null !== $node->visibilityToken && (string)$node->getName() === $name) {
+                yield new DocumentHighlight(
+                    new Range(
+                        PositionConverter::intByteOffsetToPosition($node->variableName->getStartPosition(), $node->getFileContents()),
+                        PositionConverter::intByteOffsetToPosition($node->variableName->getEndPosition(), $node->getFileContents())
+                    ),
+                    DocumentHighlightKind::TEXT
+                );
+                continue;
+            }
+
+            if ($node instanceof Variable && $node->getFirstAncestor(PropertyDeclaration::class) && (string)$node->getName() === $name) {
                 yield new DocumentHighlight(
                     new Range(
                         PositionConverter::intByteOffsetToPosition($node->getStartPosition(), $node->getFileContents()),
