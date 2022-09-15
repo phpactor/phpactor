c97139a2e8a23ee87bb8f26f97ff5478e749a658
Daniel Leech <dan.t.leech@gmail.com>
2022-09-15 19:28:46 +0200

    Changes

--- lib/Search/Adapter/WorseReflection/WorseMatchFilter.php
+++ lib/Search/Adapter/WorseReflection/WorseMatchFilter.php
@@ -6,6 +6,8 @@ use Microsoft\PhpParser\Parser;
 use Phpactor\Search\Model\DocumentMatches;
 use Phpactor\Search\Model\MatchFilter;
 use Phpactor\WorseReflection\Core\Reflector\SourceCodeReflector;
+use Phpactor\WorseReflection\Core\Type\BooleanType;
+use RuntimeException;
 
 class WorseMatchFilter implements MatchFilter
 {
@@ -27,13 +29,26 @@ class WorseMatchFilter implements MatchFilter
         foreach ($matches as $match) {
             $vars = [];
             foreach ($match->tokens() as $name => $token) {
-                $vars[$name] = new TypedMatchToken($name, $token, $this->reflector->reflectOffset(
-                    $matches->document(),
-                    $token->range->start()->toInt()
-                )->symbolContext()->type());
+                // only do static analysis for tokens that are placeholders.
+                //
+                // TODO: don't rely name being a string to decide if this is a
+                //       placeholder or not.
+                if (is_string($name)) {
+                    $vars[$name] = new TypedMatchToken($name, $token, $this->reflector->reflectOffset(
+                        $matches->document(),
+                        $token->range->start()->toInt()
+                    )->symbolContext()->type());
+                }
             }
 
-            if ($this->evaluator->evaluate($expression, $vars)->isFalse()) {
+            $result = $this->evaluator->evaluate($expression, new TypedMatchTokens($vars));
+
+            if (!$result instanceof BooleanType) {
+                throw new RuntimeException(sprintf(
+                    'Filter must evaluate to a boolean, got "%s"', $result->__toString()
+                ));
+            }
+            if (false === $result->isTrue()) {
                 continue;
             }
             $filtered[] = $match;
