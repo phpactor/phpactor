4a1f570a5d4a306c0dfcc7d5e9757d0a0f1cb84f
Daniel Leech <dan.t.leech@gmail.com>
2022-10-10 21:43:29 +0200

    Support for class-string generic

--- lib/WorseReflection/Core/Inference/GenericMapResolver.php
+++ lib/WorseReflection/Core/Inference/GenericMapResolver.php
@@ -12,6 +12,7 @@ use Phpactor\WorseReflection\Core\TypeFactory;
 use Phpactor\WorseReflection\Core\Type\ClassStringType;
 use Phpactor\WorseReflection\Core\Type\ClassType;
 use Phpactor\WorseReflection\Core\Type\GenericClassType;
+use Phpactor\WorseReflection\Core\Type\StringLiteralType;
 
 class GenericMapResolver
 {
@@ -102,9 +103,15 @@ class GenericMapResolver
             return;
         }
         $classStringType = $type->className()->short();
-        dd($classStringType);
         if ($templateMap->has($classStringType)) {
-            $templateMap->replace($classStringType, $argument);
+            if ($argument instanceof ClassStringType) {
+                $templateMap->replace($classStringType, TypeFactory::class($argument->className()));
+            }
+            if ($argument instanceof StringLiteralType) {
+                $templateMap->replace($classStringType, TypeFactory::class($argument->value()));
+            }
+
+            return;
         }
     }
 }
