33a9bf20e93b0e3dda8a28aa9599712822bbf92a
Daniel Leech <dan.t.leech@gmail.com>
2022-12-24 16:19:59 +0000

    Failing test for update

--- lib/CodeTransform/Tests/Adapter/WorseReflection/Transformer/UpdateDocblockTransformerTest.php
+++ lib/CodeTransform/Tests/Adapter/WorseReflection/Transformer/UpdateDocblockTransformerTest.php
@@ -631,6 +631,49 @@ class UpdateDocblockTransformerTest extends WorseTestCase
                 }
                 EOT
         ];
+
+        yield 'updates existing docblock' => [
+            <<<'EOT'
+                <?php
+
+                namespace Foo;
+
+                trait Foobar {
+                    /**
+                     */
+                    public function baz(): array
+                    {
+                        yield [
+                            'foobar',
+                            function (Bar $b): string {
+                            }
+                        ];
+                    }
+                }
+                EOT
+            ,
+            <<<'EOT'
+                <?php
+
+                namespace Foo;
+
+                use Generator;
+
+                trait Foobar {
+                    /**
+                     * @return Generator<array{string,Closure(Bar): string}>
+                     */
+                    public function baz(): array
+                    {
+                        yield [
+                            'foobar',
+                            function (Bar $b): string {
+                            }
+                        ];
+                    }
+                }
+                EOT
+        ];
     }
 
     /**
