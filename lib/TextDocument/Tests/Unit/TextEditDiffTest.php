b1dd92ff2fe158745476edeb3e95c74a550441a4
dantleech <dan.t.leech@gmail.com>
2022-12-30 17:16:16 +0000

    Attempting to make an algorithm to produce the text edits from document A to B (#2005)
    * Failing to make a diff
    
    * Types
    
    * Text edits
    
    * Works
    
    * CS fixes

---
+++ lib/TextDocument/Tests/Unit/TextEditDiffTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Phpactor\TextDocument\Tests\Unit;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+use Phpactor\TextDocument\TextEditDiff;
+
+class TextEditDiffTest extends TestCase
+{
+    /**
+     * @dataProvider provideDiff
+     */
+    public function testDiff(string $one, string $two): void
+    {
+        $edits = (new TextEditDiff())->diff($one, $two);
+        self::assertEquals(
+            $two,
+            $edits->apply($one)
+        );
+    }
+
+    /**
+      * @return Generator<string,array{string,string}>
+      */
+    public function provideDiff(): Generator
+    {
+        yield 'add string' => [
+            'foo',
+            'foo bar',
+        ];
+        yield 'remove string' => [
+            'foo bar',
+            'foo',
+        ];
+        yield 'insert string' => [
+            'foo bar',
+            'foo baz boo bar bag',
+        ];
+
+        yield 'first char' => [
+            'i',
+            'b',
+        ];
+
+        yield 'differnet' => [
+            'it little profits',
+            'that an idle king',
+        ];
+
+        yield 'poem' => [
+            implode("\n", [
+                'it little profits that an idle king',
+                'matched with an aged wife',
+            ]),
+            implode("\n", [
+                'by this still hearth',
+                'it little profits that an idle king',
+            ])
+        ];
+
+        yield 'code' => [
+            implode("\n", [
+                '<?php',
+                'class Foobar {',
+                '    private $foobar;',
+                '    /**',
+                '     * @return array<int,foobar>',
+                '     */',
+                '    public function bar() {',
+                '    }',
+                '}'
+            ]),
+            implode("\n", [
+                '<?php',
+                'class Foobar {',
+                '',
+                '    /**',
+                '     * @var Foobars',
+                '     */',
+                '    private $foobar;',
+                '',
+                '    /**',
+                '     * @param array<int,foobar> $bar',
+                '     * @return array<string,foobar>',
+                '     */',
+                '    public function bar(array $bar) {',
+                '    }',
+                '}'
+            ]),
+        ];
+    }
+}
