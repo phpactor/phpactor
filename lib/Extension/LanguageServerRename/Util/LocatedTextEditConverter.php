de20e176e1663e341864242f2275185df2e5b2a7
Daniel Leech <dan.t.leech@gmail.com>
2023-02-11 13:11:47 +0000

    Fix rename

--- lib/Extension/LanguageServerRename/Util/LocatedTextEditConverter.php
+++ lib/Extension/LanguageServerRename/Util/LocatedTextEditConverter.php
@@ -3,6 +3,7 @@
 namespace Phpactor\Extension\LanguageServerRename\Util;
 
 use Phpactor\Extension\LanguageServerBridge\Converter\TextEditConverter;
+use Phpactor\LanguageServerProtocol\OptionalVersionedTextDocumentIdentifier;
 use Phpactor\Rename\Model\LocatedTextEditsMap;
 use Phpactor\Rename\Model\RenameResult;
 use Phpactor\LanguageServerProtocol\RenameFile;
@@ -24,9 +25,9 @@ final class LocatedTextEditConverter
         foreach ($map->toLocatedTextEdits() as $result) {
             $version = $this->getDocumentVersion((string)$result->documentUri());
             $documentEdits[] = new TextDocumentEdit(
-                new VersionedTextDocumentIdentifier(
-                    (string)$result->documentUri(),
-                    $version
+                new OptionalVersionedTextDocumentIdentifier(
+                    uri: (string)$result->documentUri(),
+                    version: $version,
                 ),
                 TextEditConverter::toLspTextEdits(
                     $result->textEdits(),
