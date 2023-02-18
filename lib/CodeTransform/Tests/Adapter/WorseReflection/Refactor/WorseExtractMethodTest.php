4cdeee1cfffc8463006681a961843bc0af5a69f7
mamazu <14860264+mamazu@users.noreply.github.com>
2023-02-18 17:43:30 +0100

    Making extractMethod from static function also generate static function (#2101)
    * Making method from static also generate static functions
    
    * Making tests more modular

--- lib/CodeTransform/Tests/Adapter/WorseReflection/Refactor/WorseExtractMethodTest.php
+++ lib/CodeTransform/Tests/Adapter/WorseReflection/Refactor/WorseExtractMethodTest.php
@@ -8,20 +8,16 @@ use Phpactor\CodeTransform\Domain\SourceCode;
 use Phpactor\WorseReflection\Core\SourceCode as WorseSourceCode;
 use Phpactor\CodeBuilder\Adapter\WorseReflection\WorseBuilderFactory;
 use Exception;
+use Generator;
 
 class WorseExtractMethodTest extends WorseTestCase
 {
     /**
      * @dataProvider provideExtractMethod
      */
-    public function testExtractMethod(string $test, string $name, ?string $expectedExceptionMessage = null): void
+    public function testExtractMethod(string $test): void
     {
         [$source, $expected, $offsetStart, $offsetEnd] = $this->sourceExpectedAndOffset(__DIR__ . '/fixtures/' . $test);
-
-        if ($expectedExceptionMessage) {
-            $this->expectException(Exception::class);
-            $this->expectExceptionMessage($expectedExceptionMessage);
-        }
 
         $worseSourceCode = WorseSourceCode::fromPathAndString('file:///source', $source);
         $reflector = $this->reflectorForWorkspace($worseSourceCode);
@@ -30,7 +26,7 @@ class WorseExtractMethodTest extends WorseTestCase
         $extractMethod = new WorseExtractMethod($reflector, $factory, $this->updater());
 
         $sourceCode = SourceCode::fromStringAndPath($source, 'file:///source');
-        $textDocumentEdits = $extractMethod->extractMethod($sourceCode, $offsetStart, $offsetEnd, $name);
+        $textDocumentEdits = $extractMethod->extractMethod($sourceCode, $offsetStart, $offsetEnd, 'newMethod');
 
         $transformed = SourceCode::fromStringAndPath(
             (string) $textDocumentEdits->textEdits()->apply($sourceCode),
@@ -39,154 +35,69 @@ class WorseExtractMethodTest extends WorseTestCase
         $this->assertEquals(trim($expected), trim($transformed));
     }
 
-    public function provideExtractMethod(): array
+    /** @return Generator<array<string>> */
+    public function provideExtractMethod(): Generator
     {
-        return [
-            'no free variables' => [
-                'extractMethod1.test',
-                'newMethod'
-            ],
-            'free variable' => [
-                'extractMethod2.test',
-                'newMethod'
-            ],
-            'free variables' => [
-                'extractMethod3.test',
-                'newMethod'
-            ],
-            'namespaced' => [
-                'extractMethod4.test',
-                'newMethod'
-            ],
-            'duplicated vars' => [
-                'extractMethod5.test',
-                'newMethod'
-            ],
-            'return value and assignment' => [
-                'extractMethod6.test',
-                'newMethod'
-            ],
-            'multiple return value and assignment' => [
-                'extractMethod7.test',
-                'newMethod'
-            ],
-            'multiple return value with incoming variables' => [
-                'extractMethod8.test',
-                'newMethod'
-            ],
-            'multiple return value boundaries' => [
-                'extractMethod10.test',
-                'newMethod',
-            ],
-            'method exists' => [
-                'extractMethod9.test',
-                'newMethod',
-                'Class "extractMethod" already has method "newMethod"'
-            ],
-            'tail variables are taken from scope' => [
-                'extractMethod11.test',
-                'newMethod',
-            ],
-            'replacement indentation is preserved' => [
-                'extractMethod12.test',
-                'newMethod',
-            ],
-            'only considers selection content for return vars' => [
-                'extractMethod13.test',
-                'newMethod',
-            ],
-            'return mutated primative' => [
-                'extractMethod14.test',
-                'newMethod',
-            ],
-            'imports classes' => [
-                'extractMethod15.test',
-                'newMethod',
-            ],
-            'adds return type for scalar' => [
-                'extractMethod16.test',
-                'newMethod',
-            ],
-            'adds return type for nullable scalar' => [
-                'extractMethod16A.test',
-                'newMethod',
-            ],
-            'adds return type and import for nullable class' => [
-                'extractMethod16B.test',
-                'newMethod',
-            ],
-            'adds return type for class' => [
-                'extractMethod17.test',
-                'newMethod',
-            ],
-            'extracts expression to method' => [
-                'extractMethod18.test',
-                'newMethod',
-            ],
-            'extracts assignment expression to method' => [
-                'extractMethod19.test',
-                'newMethod',
-            ],
-            'extracts assignment expression with unknown return type' => [
-                'extractMethod20.test',
-                'newMethod',
-            ],
-            'extract expression and adds short return type for class' => [
-                'extractMethod21.test',
-                'newMethod',
-            ],
-            'return if extracted code has a return' => [
-                'extractMethod22.test',
-                'newMethod',
-            ],
-            'adds method to declaring class' => [
-                'extractMethod23.test',
-                'newMethod',
-            ],
-            'different scopes 1' => [
-                'extractMethod24.test',
-                'newMethod',
-                'Cannot extract method. Check if start and end statements are in different scopes.'
-            ],
-            'different scopes 2' => [
-                'extractMethod25.test',
-                'newMethod',
-                'Cannot extract method. Check if start and end statements are in different scopes.'
-            ],
-            'different scopes 3' => [
-                'extractMethod26.test',
-                'newMethod',
-                'Cannot extract method. Check if start and end statements are in different scopes.'
-            ],
-            'empty text selection' => [
-                'extractMethod27.test',
-                'newMethod',
-            ],
-            'empty selection 2' => [
-                'extractMethod28.test',
-                'newMethod',
-                'Cannot extract method. Check if start and end statements are in different scopes.'
-            ],
-            'nullable argument' => [
-                'extractMethod29.test',
-                'newMethod',
-            ],
-            'ignore scoped variables: catch clause' => [
-                'extractMethod30.test',
-                'newMethod',
-            ],
-            'ignore scoped variables: anonymous function' => [
-                'extractMethod31.test',
-                'newMethod',
-            ],
-            'union argument' => [
-                'extractMethod32.test',
-                'newMethod',
-            ],
-            'extract method from trait' => [
-                'extractMethod33.test',
-                'newMethod'
-            ],
+        yield 'no free variables' => ['extractMethod1.test'];
+        yield 'free variable' => ['extractMethod2.test'];
+        yield 'free variables' => ['extractMethod3.test'];
+        yield 'namespaced' => ['extractMethod4.test'];
+        yield 'duplicated vars' => ['extractMethod5.test'];
+        yield 'return value and assignment' => ['extractMethod6.test'];
+        yield 'multiple return value and assignment' => ['extractMethod7.test'];
+        yield 'multiple return value with incoming variables' => ['extractMethod8.test'];
+        yield 'multiple return value boundaries' => ['extractMethod10.test'];
+        yield 'tail variables are taken from scope' => ['extractMethod11.test'];
+        yield 'replacement indentation is preserved' => ['extractMethod12.test'];
+        yield 'only considers selection content for return vars' => ['extractMethod13.test'];
+        yield 'return mutated primative' => ['extractMethod14.test'];
+        yield 'imports classes' => ['extractMethod15.test'];
+        yield 'adds return type for scalar' => ['extractMethod16.test'];
+        yield 'adds return type for nullable scalar' => ['extractMethod16A.test'];
+        yield 'adds return type and import for nullable class' => ['extractMethod16B.test'];
+        yield 'adds return type for class' => ['extractMethod17.test'];
+        yield 'extracts expression to method' => ['extractMethod18.test'];
+        yield 'extracts assignment expression to method' => ['extractMethod19.test'];
+        yield 'extracts assignment expression with unknown return type' => ['extractMethod20.test'];
+        yield 'extract expression and adds short return type for class' => ['extractMethod21.test'];
+        yield 'return if extracted code has a return' => ['extractMethod22.test'];
+        yield 'adds method to declaring class' => ['extractMethod23.test'];
+        yield 'empty text selection' => ['extractMethod27.test'];
+        yield 'nullable argument' => ['extractMethod29.test'];
+        yield 'ignore scoped variables: catch clause' => ['extractMethod30.test'];
+        yield 'ignore scoped variables: anonymous function' => ['extractMethod31.test'];
+        yield 'union argument' => ['extractMethod32.test'];
+        yield 'extract method from trait' => ['extractMethod33.test'];
+        yield 'extract static method' => ['extractMethod34.test'];
+    }
+
+    /**
+     * @dataProvider provideExtractMethodFromDifferentScopes
+     */
+    public function testExtractingMethodsFromDifferentScopes(string $test): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Cannot extract method. Check if start and end statements are in different scopes.');
+
+        $this->testExtractMethod($test);
+    }
+
+    /** @return Generator<array<string>> */
+    public function provideExtractMethodFromDifferentScopes(): Generator
+    {
+        yield['extractMethod24.test'];
+        yield['extractMethod25.test'];
+        yield['extractMethod26.test'];
+        yield 'empty selection 2' => [
+            'extractMethod28.test',
         ];
+    }
+
+    public function testExtractMethodThatExists(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Class "extractMethod" already has method "newMethod"');
+
+        $this->testExtractMethod('extractMethod_methodExists.test');
     }
 }
