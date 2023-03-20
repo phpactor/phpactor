734909f3133e86db02d092bfb313812de4c9a369
Daniel Leech <daniel@dantleech.com>
2023-03-20 10:53:17 +0000

    Removed dead

--- lib/WorseReflection/Core/Position.php
+++ lib/WorseReflection/Core/Position.php
@@ -4,18 +4,13 @@ namespace Phpactor\WorseReflection\Core;
 
 final class Position
 {
-    private function __construct(private int $fullStart, private int $start, private int $end)
+    private function __construct(private int $start, private int $end)
     {
     }
 
-    public static function fromInts(int $start, int $end)
+    public static function fromInts(int $start, int $end): self
     {
-        return new self($start, $start, $end);
-    }
-
-    public function fullStart(): int
-    {
-        return $this->fullStart;
+        return new self($start, $end);
     }
 
     public function start(): int
@@ -31,10 +26,5 @@ final class Position
     public function width(): int
     {
         return $this->end - $this->start;
-    }
-
-    public function fullWidth(): int
-    {
-        return $this->end - $this->fullStart;
     }
 }
