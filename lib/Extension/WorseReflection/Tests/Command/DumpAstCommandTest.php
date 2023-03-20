791c93d3c7555328441a81c92e86096e80c30311
Daniel Leech <daniel@dantleech.com>
2023-03-20 21:18:42 +0000

    Add command to dump and profile parsing

---
+++ lib/Extension/WorseReflection/Tests/Command/DumpAstCommandTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Phpactor\Extension\WorseReflection\Tests\Command;
+
+use Microsoft\PhpParser\Parser;
+use Phpactor\Extension\WorseReflection\Command\DumpAstCommand;
+use Phpactor\TestUtils\PHPUnit\TestCase;
+use Phpactor\TestUtils\Workspace;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class DumpAstCommandTest extends TestCase
+{
+    public function testDumpAstCommand(): void
+    {
+        $output = new BufferedOutput();
+        $workspace = new Workspace(__DIR__ . '/../Workspace');
+        $workspace->reset();
+        $workspace->put('test.php', '<?php echo "hello";');
+        $exitCode = (new DumpAstCommand(new Parser()))->run(new ArrayInput([
+            'path' => $workspace->path('test.php')
+        ]), $output);
+        self::assertEquals(0, $exitCode);
+        self::assertStringContainsString('Parsing time', $output->fetch());
+
+    }
+}
