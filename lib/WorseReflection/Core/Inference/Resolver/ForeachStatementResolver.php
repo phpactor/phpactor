c12567cceb4bb5a37a5908c4a63d8328bb004e8b
Daniel Leech <daniel@dantleech.com>
2023-03-21 16:24:25 +0000

    UPdate

--- lib/WorseReflection/Core/Inference/Resolver/ForeachStatementResolver.php
+++ lib/WorseReflection/Core/Inference/Resolver/ForeachStatementResolver.php
@@ -38,8 +38,6 @@ class ForeachStatementResolver implements Resolver
 
         $this->addAssignedVarsInCompoundStatement($node, $resolver, $frame);
 
-        $resolver->resolveNode($frame, $node->statements);
-
         return $context;
     }
 
