f7e2583b5c7bf30ff173356a845d3c0276f0a25a
Daniel Leech <daniel@dantleech.com>
2023-03-26 11:20:15 +0100

    Replaced with real TD

--- lib/TextDocument/TextDocument.php
+++ lib/TextDocument/TextDocument.php
@@ -2,99 +2,24 @@
 
 namespace Phpactor\TextDocument;
 
-use Phpactor\TextDocument\TextDocument;
-use Phpactor\TextDocument\TextDocumentLanguage;
-use Phpactor\TextDocument\TextDocumentUri;
-use InvalidArgumentException;
-use RuntimeException;
-
-class TextDocument implements TextDocument
+/**
+ * Represents source code or other text documents.
+ */
+interface TextDocument
 {
-    private function __construct(private string $source, private ?string $path = null)
-    {
-    }
+    /**
+     * Return the document as a string
+     */
+    public function __toString();
 
-    public function __toString(): string
-    {
-        return $this->source;
-    }
+    /**
+     * Return the URI to the document or NULL if the document has not been
+     * persisted yet.
+     */
+    public function uri(): ?TextDocumentUri;
 
-    public static function fromUnknown(TextDocument|TextDocument|string $value): TextDocument
-    {
-        if ($value instanceof TextDocument) {
-            return $value;
-        }
-
-        if ($value instanceof TextDocument) {
-            if (null === $value->uri()) {
-                return self::fromString(
-                    $value->__toString()
-                );
-            }
-            return self::fromPathAndString(
-                $value->uri()->path(),
-                $value->__toString()
-            );
-        }
-
-        return self::fromString($value);
-    }
-
-    public static function fromString(string $source): self
-    {
-        return new self($source);
-    }
-
-    public static function fromPath(string $filePath): self
-    {
-        if (!file_exists($filePath)) {
-            throw new InvalidArgumentException(sprintf(
-                'File "%s" does not exist',
-                $filePath
-            ));
-        }
-
-        return new self((string) file_get_contents($filePath), $filePath);
-    }
-
-    public static function empty(): self
-    {
-        return new self('');
-    }
-
-    public static function fromPathAndString(string $filePath, string $source): self
-    {
-        return new self($source, $filePath);
-    }
-
-    public function uri(): ?TextDocumentUri
-    {
-        if (!$this->path) {
-            return null;
-        }
-
-        return TextDocumentUri::fromString($this->path);
-    }
-
-    public function mustGetUri(): TextDocumentUri
-    {
-        $uri = $this->uri();
-        if (null === $uri) {
-            throw new RuntimeException(
-                'URI has not been set on source code'
-            );
-        }
-
-        return $uri;
-    }
-
-    public function language(): TextDocumentLanguage
-    {
-        return TextDocumentLanguage::fromString('php');
-    }
-
-    public function path(): ?string
-    {
-        return $this->path;
-    }
+    /**
+     * Return language value object for the text document.
+     */
+    public function language(): TextDocumentLanguage;
 }
