2e17adaf29beafd92e53f5ff65d8e96e0431be41
Daniel Leech <daniel@dantleech.com>
2023-03-26 14:16:41 +0100

    Fix diagnostics

--- lib/Extension/LanguageServerWorseReflection/DiagnosticProvider/WorseDiagnosticProvider.php
+++ lib/Extension/LanguageServerWorseReflection/DiagnosticProvider/WorseDiagnosticProvider.php
@@ -4,6 +4,7 @@ namespace Phpactor\Extension\LanguageServerWorseReflection\DiagnosticProvider;
 
 use Amp\CancellationToken;
 use Amp\Promise;
+use Phpactor\Extension\LanguageServerBridge\Converter\TextDocumentConverter;
 use Phpactor\LanguageServerProtocol\DiagnosticSeverity as LanguageServerProtocolDiagnosticSeverity;
 use Phpactor\Extension\LanguageServerBridge\Converter\PositionConverter;
 use Phpactor\LanguageServerProtocol\Range;
@@ -24,7 +25,7 @@ class WorseDiagnosticProvider implements DiagnosticsProvider
     {
         return call(function () use ($textDocument, $cancel) {
             $lspDiagnostics = [];
-            foreach ($this->reflector->diagnostics($textDocument->text) as $diagnostic) {
+            foreach ($this->reflector->diagnostics(TextDocumentConverter::fromLspTextItem($textDocument)) as $diagnostic) {
                 $range = new Range(
                     PositionConverter::byteOffsetToPosition($diagnostic->range()->start(), $textDocument->text),
                     PositionConverter::byteOffsetToPosition($diagnostic->range()->end(), $textDocument->text),
