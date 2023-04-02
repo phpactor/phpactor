64cc610d539cda13fa8addf99629772691fcba67
Daniel Leech <daniel@dantleech.com>
2023-04-02 19:26:06 +0100

    Update generate method provider etc

--- lib/CodeTransform/Adapter/WorseReflection/Helper/WorseMissingMethodFinder.php
+++ lib/CodeTransform/Adapter/WorseReflection/Helper/WorseMissingMethodFinder.php
@@ -7,6 +7,7 @@ use Phpactor\CodeTransform\Domain\Helper\MissingMethodFinder\MissingMethod;
 use Phpactor\TextDocument\TextDocument;
 use Phpactor\WorseReflection\Bridge\TolerantParser\Diagnostics\MissingMethodDiagnostic;
 use Phpactor\WorseReflection\Reflector;
+use function Amp\call;
 
 class WorseMissingMethodFinder implements MissingMethodFinder
 {
@@ -15,19 +16,21 @@ class WorseMissingMethodFinder implements MissingMethodFinder
     }
 
 
-    public function find(TextDocument $sourceCode): array
+    public function find(TextDocument $sourceCode): Promise<array>
     {
-        $diagnostics = $this->reflector->diagnostics($sourceCode)->byClass(MissingMethodDiagnostic::class);
-        $missing = [];
+        return call(function () use ($sourceCode) {
+            $diagnostics = $this->reflector->diagnostics($sourceCode)->byClass(MissingMethodDiagnostic::class);
+            $missing = [];
 
-        /** @var MissingMethodDiagnostic $missingMethod */
-        foreach ($diagnostics as $missingMethod) {
-            $missing[] = new MissingMethod(
-                $missingMethod->methodName(),
-                $missingMethod->range(),
-            );
-        }
+            /** @var MissingMethodDiagnostic $missingMethod */
+            foreach ($diagnostics as $missingMethod) {
+                $missing[] = new MissingMethod(
+                    $missingMethod->methodName(),
+                    $missingMethod->range(),
+                );
+            }
 
-        return $missing;
+            return $missing;
+        });
     }
 }
