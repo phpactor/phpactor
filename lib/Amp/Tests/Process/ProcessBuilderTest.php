b2938a1b15b24f8cce7182a60854bec23ce77545
Daniel Leech <daniel@dantleech.com>
2023-04-15 22:19:53 +0100

    Add AMP process builder

---
+++ lib/Amp/Tests/Process/ProcessBuilderTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Phpactor\Amp\Tests\Process;
+
+use PHPUnit\Framework\TestCase;
+use Phpactor\Amp\Process\ProcessBuilder;
+use function Amp\ByteStream\buffer;
+use function Amp\Promise\wait;
+
+class ProcessBuilderTest extends TestCase
+{
+    const PARENT_PROCESS_ENV_VAR = 'thisistheparentprocess';
+
+    public function testBuildProcess(): void
+    {
+        $process = ProcessBuilder::create(['export'])->build();
+        $pid = wait($process->start());
+        $exitCode = wait($process->join());
+        self::assertEquals(0, $exitCode);
+    }
+
+    public function testDoesNotMergeEnvByDefaultWhenEnvVarsPassed(): void
+    {
+        putenv('FOO='.self::PARENT_PROCESS_ENV_VAR);
+        $process = ProcessBuilder::create(['export'])->env(['ENV'=> 'myenvvar'])->build();
+        $pid = wait($process->start());
+        $exitCode = wait($process->join());
+        self::assertEquals(0, $exitCode);
+        $out = wait(buffer($process->getStdout()));
+        self::assertStringContainsString('myenvvar', $out);
+        self::assertStringNotContainsString(self::PARENT_PROCESS_ENV_VAR, $out);
+        putenv('FOO');
+    }
+
+    public function testInheritsWhenInstructedToWhenEnvVarsPassed(): void
+    {
+        putenv('FOO='.self::PARENT_PROCESS_ENV_VAR);
+        $process = ProcessBuilder::create(['export'])->env(['ENV'=> 'myenvvar'])->mergeParentEnv()->build();
+        $pid = wait($process->start());
+        $exitCode = wait($process->join());
+        self::assertEquals(0, $exitCode);
+        $out = wait(buffer($process->getStdout()));
+        self::assertStringContainsString('myenvvar', $out);
+        self::assertStringContainsString(self::PARENT_PROCESS_ENV_VAR, $out);
+        putenv('FOO');
+    }
+}
