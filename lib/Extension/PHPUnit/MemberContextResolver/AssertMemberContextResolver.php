5d2a024c98fd5d1dd8212001316a978583e91458
mamazu <14860264+mamazu@users.noreply.github.com>
2023-05-06 13:38:09 +0200

    Failing test

--- lib/Extension/PHPUnit/MemberContextResolver/AssertMemberContextResolver.php
+++ lib/Extension/PHPUnit/MemberContextResolver/AssertMemberContextResolver.php
@@ -7,17 +7,24 @@ use Phpactor\WorseReflection\Core\Inference\FunctionArguments;
 use Phpactor\WorseReflection\Core\Inference\Resolver\MemberAccess\MemberContextResolver;
 use Phpactor\WorseReflection\Core\Reflection\ReflectionMember;
 use Phpactor\WorseReflection\Core\Type;
+use Phpactor\WorseReflection\Core\TypeFactory;
 use Phpactor\WorseReflection\Reflector;
+use Phpactor\WorseReflection\Core\Type\StringLiteralType;
 
 class AssertMemberContextResolver implements MemberContextResolver
 {
     public function resolveMemberContext(Reflector $reflector, ReflectionMember $member, Type $type, ?FunctionArguments $arguments): ?Type
     {
+        dd('I am here!');
         if ($member->memberType() !== ReflectionMember::TYPE_METHOD) {
             return null;
         }
 
         if ($member->name() !== 'assertInstanceOf') {
+            return null;
+        }
+
+        if (!$member->class()->isInstanceOf(ClassName::fromString('PHPUnit\Framework\Assert'))) {
             return null;
         }
 
@@ -29,12 +36,12 @@ class AssertMemberContextResolver implements MemberContextResolver
             return null;
         }
 
-        if (!$member->class()->isInstanceOf(ClassName::fromString('PHPUnit\Framework\Assert'))) {
+        $argument = $arguments->at(0);
+
+        if (!$argument instanceof StringLiteralType) {
             return null;
         }
 
-        $argument = $arguments->at(0);
-
-        return null;
+        return TypeFactory::fromStringWithReflector($argument->value(), $reflector);
     }
 }
---
+++ lib/Extension/PHPUnit/Tests/Unit/MemberContextResolver/AssertMemberContextResolverTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpactor\Extension\PHPUnit\Tests\Unit\MemberContextResolver;
+
+use Phpactor\Tests\IntegrationTestCase;
+use Phpactor\Extension\PHPUnit\MemberContextResolver\AssertMemberContextResolver;
+use Phpactor\TextDocument\TextDocumentBuilder;
+use Phpactor\WorseReflection\ReflectorBuilder;
+use Phpactor\WorseReflection\Core\Inference\Walker\TestAssertWalker;
+
+class AssertMemberContextResolverTest extends IntegrationTestCase
+{
+    public function testResolveClassString(): void {
+        $this->resolve(
+            <<<'EOT'
+            <?php
+
+            class SomeTest extends PHPUnit\Framework\Assert {
+                public function test(): void {
+                    $this->assertInstanceOf('SomeClass', $obj);
+                    wrAssertType('SomeClass', $obj);
+                }
+            }
+            EOT
+        );
+    }
+
+    public function resolve(string $sourceCode): void
+    {
+        $sourceCode = TextDocumentBuilder::fromUnknown($sourceCode);
+        $reflector = ReflectorBuilder::create()
+                ->addFrameWalker(new TestAssertWalker($this))
+                ->addMemberContextResolver(new AssertMemberContextResolver())
+        ->build();
+
+        $reflector->reflectOffset($sourceCode, mb_strlen((string) $sourceCode));
+    }
+}
