424877faf3777a2b2568004fa7f13e1a3b82721c
Daniel <daniel@dantleech.com>
2023-06-04 09:55:22 +0100

    Fix optinoals

--- lib/Application.php
+++ lib/Application.php
@@ -23,7 +23,7 @@ class Application extends SymfonyApplication
 {
     private Container $container;
 
-    public function __construct(private string $vendorDir, private string $phpactorBin)
+    public function __construct(private string $vendorDir, private ?string $phpactorBin = null)
     {
         parent::__construct('Phpactor', Cast::toString(Versions::getVersion('phpactor/phpactor')));
     }
