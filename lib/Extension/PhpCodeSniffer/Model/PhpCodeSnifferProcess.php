279c7ed142f1884bd0c46727b9edfb7cd6df31bf
Daniel <daniel@dantleech.com>
2023-09-10 12:34:50 +0100

    Remove unreachable return

--- lib/Extension/PhpCodeSniffer/Model/PhpCodeSnifferProcess.php
+++ lib/Extension/PhpCodeSniffer/Model/PhpCodeSnifferProcess.php
@@ -148,7 +148,6 @@ class PhpCodeSnifferProcess
                         $stdout
                     )
                 );
-                return '[]';
             }
 
             return $stdout;
