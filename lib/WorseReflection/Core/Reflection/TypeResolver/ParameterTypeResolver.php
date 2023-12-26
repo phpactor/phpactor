545e4267d91eb4d4ea58cae3a66afbb2de0121f0
Daniel Leech <daniel@dantleech.com>
2023-12-26 18:50:40 +0000

    Support type inheritance

--- lib/WorseReflection/Core/Reflection/TypeResolver/ParameterTypeResolver.php
+++ lib/WorseReflection/Core/Reflection/TypeResolver/ParameterTypeResolver.php
@@ -2,6 +2,9 @@
 
 namespace Phpactor\WorseReflection\Core\Reflection\TypeResolver;
 
+use Phpactor\WorseReflection\Core\ClassHierarchyResolver;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionFunctionLike;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionMethod;
 use Phpactor\WorseReflection\Core\Type;
 use Phpactor\WorseReflection\Core\Reflection\ReflectionParameter;
 use Phpactor\WorseReflection\TypeUtil;
@@ -14,9 +17,36 @@ class ParameterTypeResolver
 
     public function resolve(): Type
     {
-        $docblock = $this->parameter->functionLike()->docblock();
-        $docblockType = $docblock->parameterType($this->parameter->name());
+        $functionLike = $this->parameter->functionLike();
+        $type = $this->resolveType($functionLike);
 
-        return TypeUtil::firstDefined($docblockType, $this->parameter->type());
+        return $type;
+    }
+
+    public function resolveType(ReflectionFunctionLike $functionLike): Type
+    {
+        if (!$functionLike instanceof ReflectionMethod) {
+            $docblock = $this->parameter->functionLike()->docblock();
+            $docblockType = $docblock->parameterType($this->parameter->name());
+            return TypeUtil::firstDefined($docblockType, $this->parameter->type());
+        }
+
+        $hierarchy = (new ClassHierarchyResolver(
+            ClassHierarchyResolver::INCLUDE_PARENT | ClassHierarchyResolver::INCLUDE_INTERFACE
+        ))->resolve($functionLike->class());
+
+        foreach ($hierarchy as $classLike) {
+            if (!$classLike->methods()->has($functionLike->name())) {
+                continue;
+            }
+            $docblock = $classLike->methods()->get($functionLike->name())->docblock();
+            $type = $docblock->parameterType($this->parameter->name());
+            if ($type->isDefined()) {
+                return $type;
+            }
+        }
+
+        return $this->parameter->type();
+
     }
 }
