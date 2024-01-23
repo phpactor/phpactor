0474a540ee24d12da8ec04e28f515a6f81b06e9a
przepompownia <przepompownia@users.noreply.github.com>
2024-01-23 23:49:53 +0100

    Fix ScfClassCompletorTest

--- lib/Completion/Bridge/TolerantParser/SourceCodeFilesystem/ScfClassCompletor.php
+++ lib/Completion/Bridge/TolerantParser/SourceCodeFilesystem/ScfClassCompletor.php
@@ -4,6 +4,7 @@ namespace Phpactor\Completion\Bridge\TolerantParser\SourceCodeFilesystem;
 
 use Generator;
 use Microsoft\PhpParser\Node;
+use Microsoft\PhpParser\Node\Expression\ObjectCreationExpression;
 use Microsoft\PhpParser\Node\QualifiedName;
 use Microsoft\PhpParser\ResolvedName;
 use Phpactor\ClassFileConverter\Domain\ClassName;
@@ -61,7 +62,9 @@ class ScfClassCompletor implements TolerantCompletor, TolerantQualifiable
             }
 
             foreach ($candidates as $candidate) {
-                $snippet = ($node->getText() === 'new') ? ['snippet' => $candidate->name() . '($1)$0'] : [];
+                $snippet = ($node instanceof ObjectCreationExpression || $node->parent instanceof ObjectCreationExpression)
+                    ? ['snippet' => $candidate->name() . '($1)$0']
+                    : [];
                 yield Suggestion::createWithOptions(
                     $candidate->name(),
                     $snippet + [
