d149885b428a5fc4a17cc04282210c65a490b8f8
Gerard Roche <gerardroche@users.noreply.github.com>
2024-01-25 11:04:54 +0000

    fix(tests): ansi output causes test failures
    On some terminals, e.g. gnome-terminal, kitty (ubuntu), some tests fail
    because the output of process contain color codes. For example in
    the following assertion:
    
        $this->assertStringContainsString('class ⟶Badger⟵', $process->getOutput());
    
    The characters surrounding "Badger" will be in color and so the
    assertion fails because there are color codes in the string.
    
    This patch adds `--no-ansi` to the `-php-cs-fixer` test command so that
    the output never has color codes.

--- lib/Extension/LanguageServerPhpCsFixer/Tests/PhpCsFixerTestCase.php
+++ lib/Extension/LanguageServerPhpCsFixer/Tests/PhpCsFixerTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Phpactor\Extension\LanguageServerPhpCsFixer\Tests;
 
+use Amp\Promise;
 use PHPUnit\Framework\TestCase;
 use Phpactor\Extension\LanguageServerPhpCsFixer\Model\PhpCsFixerProcess;
 use Psr\Log\NullLogger;
@@ -10,13 +11,16 @@ class PhpCsFixerTestCase extends TestCase
 {
     public function getPhpCsFixer(): PhpCsFixerProcess
     {
-        return new PhpCsFixerProcess(
-            __DIR__ . '/../../../../vendor/bin/php-cs-fixer',
-            new NullLogger(),
-            [
-                'PHP_CS_FIXER_IGNORE_ENV' => '1',
-                'XDEBUG_MODE' => 'off'
-            ],
-        );
+        return new class(__DIR__ . '/../../../../vendor/bin/php-cs-fixer', new NullLogger(), [ 'PHP_CS_FIXER_IGNORE_ENV' => '1', 'XDEBUG_MODE' => 'off' ], ) extends PhpCsFixerProcess {
+            public function fix(string $content, array $options = []): Promise
+            {
+                return parent::fix($content, array_merge($options, ['--no-ansi']));
+            }
+
+            public function describe(string $rule, array $options = []): Promise
+            {
+                return parent::describe($rule, array_merge($options, ['--no-ansi']));
+            }
+        };
     }
 }
