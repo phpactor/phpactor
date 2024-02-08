ba473240de0d071e3f7c8cbb3ca2615aadd4a1de
Daniel Leech <daniel@dantleech.com>
2024-02-08 20:40:04 +0000

    Check for PHAR

--- lib/Extension/Core/Application/Status.php
+++ lib/Extension/Core/Application/Status.php
@@ -2,6 +2,7 @@
 
 namespace Phpactor\Extension\Core\Application;
 
+use Composer\InstalledVersions;
 use Phpactor\ConfigLoader\Core\PathCandidates;
 use Phpactor\Extension\Php\Model\PhpVersionResolver;
 use Phpactor\Extension\SourceCodeFilesystem\SourceCodeFilesystemExtension;
@@ -59,6 +60,21 @@ class Status
             $diagnostics['config_files'][$configFile->path()] = file_exists($configFile->path());
         }
 
+        $diagnostics = $this->resolveVersion($diagnostics);
+
+        return $diagnostics;
+    }
+    /**
+     * @param array<string,string> $diagnostics
+     * @return array<string,string>
+     */
+    private function resolveVersion(array $diagnostics): array
+    {
+        if (\Phar::running() !== '') {
+            $diagnostics['phpactor_version'] = InstalledVersions::getVersion('phpactor/phpactor');
+            return $diagnostics;
+        }
+
         if ($path = $this->executableFinder->find('git')) {
             $process = new Process(
                 [
@@ -71,25 +87,25 @@ class Status
                 __DIR__ . '/../../../..'
             );
             $process->run();
-            $diagnostics = array_merge($diagnostics, $this->versionInfo($process));
+            return array_merge($diagnostics, $this->versionInfo($process));
         }
 
         return $diagnostics;
     }
-
+    /**
+     * @return array<string,string>
+     */
     private function versionInfo(Process $process): array
     {
         if ($process->getExitCode() !== 0) {
             return [
                 'phpactor_version' => 'ERROR: ' . $process->getErrorOutput(),
-                'phpactor_is_develop' => false,
             ];
         }
 
         if (!preg_match('{^(.*)REF(.*?)REF}', $process->getOutput(), $matches)) {
             return [
                 'phpactor_version' => $process->getOutput(),
-                'phpactor_is_develop' => false,
             ];
         }
 
