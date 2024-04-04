76f03c6e9568915b77754ce86592ec03cba74493
__ <__@__>
2024-04-05 01:40:31 +0200

    Use it (untested)

--- lib/Completion/Bridge/TolerantParser/ReferenceFinder/AttributeCompletor.php
+++ lib/Completion/Bridge/TolerantParser/ReferenceFinder/AttributeCompletor.php
@@ -4,7 +4,13 @@ namespace Phpactor\Completion\Bridge\TolerantParser\ReferenceFinder;
 
 use Generator;
 use Microsoft\PhpParser\Node;
+use Microsoft\PhpParser\Node\ClassConstDeclaration;
+use Microsoft\PhpParser\Node\MethodDeclaration;
+use Microsoft\PhpParser\Node\Parameter;
+use Microsoft\PhpParser\Node\PropertyDeclaration;
 use Microsoft\PhpParser\Node\QualifiedName;
+use Microsoft\PhpParser\Node\Statement\ClassDeclaration;
+use Microsoft\PhpParser\Node\Statement\FunctionDeclaration;
 use Phpactor\Completion\Bridge\TolerantParser\CompletionContext;
 use Phpactor\Completion\Bridge\TolerantParser\TolerantCompletor;
 use Phpactor\Completion\Core\Completor\NameSearcherCompletor;
@@ -26,8 +32,33 @@ class AttributeCompletor extends NameSearcherCompletor implements TolerantComple
             $name = NameUtil::toFullyQualified((string)$node->getResolvedName());
         }
 
-        yield from $this->completeName($name, $source->uri(), $node, NameSearcherType::ATTRIBUTE);
+        $targetNode = $node->getFirstAncestor(
+            ClassDeclaration::class,
+            FunctionDeclaration::class,
+            MethodDeclaration::class,
+            PropertyDeclaration::class,
+            ClassConstDeclaration::class,
+            Parameter::class,
+        );
+
+        yield from $this->completeName($name, $source->uri(), $node, $this->matchTargetNode($targetNode));
 
         return true;
+    }
+
+    /**
+     * @return NameSearcherType::ATTRIBUTE_TARGET_*|NameSearcherType::ATTRIBUTE
+     */
+    private function matchTargetNode(Node $node): string
+    {
+        return match($node::class) {
+            ClassDeclaration::class => NameSearcherType::ATTRIBUTE_TARGET_CLASS,
+            FunctionDeclaration::class => NameSearcherType::ATTRIBUTE_TARGET_FUNCTION,
+            MethodDeclaration::class => NameSearcherType::ATTRIBUTE_TARGET_METHOD,
+            PropertyDeclaration::class => NameSearcherType::ATTRIBUTE_TARGET_PROPERTY,
+            ClassConstDeclaration::class => NameSearcherType::ATTRIBUTE_TARGET_CLASS_CONSTANT,
+            Parameter::class => NameSearcherType::ATTRIBUTE_TARGET_PARAMETER,
+            default => NameSearcherType::ATTRIBUTE,
+        };
     }
 }
