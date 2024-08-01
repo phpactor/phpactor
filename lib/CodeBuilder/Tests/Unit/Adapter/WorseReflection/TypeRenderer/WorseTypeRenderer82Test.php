eb17033a4b07384d74d2e58ef684205ba9ee4466
mamazu <14860264+mamazu@users.noreply.github.com>
2024-08-02 00:03:31 +0200

    Fixing the type renderer

--- lib/CodeBuilder/Tests/Unit/Adapter/WorseReflection/TypeRenderer/WorseTypeRenderer82Test.php
+++ lib/CodeBuilder/Tests/Unit/Adapter/WorseReflection/TypeRenderer/WorseTypeRenderer82Test.php
@@ -7,6 +7,7 @@ use Phpactor\CodeBuilder\Adapter\WorseReflection\TypeRenderer\WorseTypeRenderer;
 use Phpactor\CodeBuilder\Adapter\WorseReflection\TypeRenderer\WorseTypeRenderer82;
 use Phpactor\WorseReflection\Core\Type\FalseType;
 use Phpactor\WorseReflection\Core\Type\MixedType;
+use Phpactor\WorseReflection\Core\Type\ObjectType;
 use Phpactor\WorseReflection\Core\Type\StringType;
 use Phpactor\WorseReflection\Core\Type\UnionType;
 
@@ -29,6 +30,10 @@ class WorseTypeRenderer82Test extends TypeRendererTestCase
         yield [
             new UnionType(new StringType(), new FalseType()),
             'string|false',
+        ];
+        yield [
+            new UnionType(new StringType(), new ObjectType()),
+            'string|object',
         ];
     }
 
