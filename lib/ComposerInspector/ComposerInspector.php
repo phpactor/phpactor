735820bb1a5af319dd5add1f46aedd1cc79ea417
mamazu <14860264+mamazu@users.noreply.github.com>
2024-10-03 16:27:09 +0200

    Adding composer_bin_dir path expander

--- lib/ComposerInspector/ComposerInspector.php
+++ lib/ComposerInspector/ComposerInspector.php
@@ -7,20 +7,26 @@ use RuntimeException;
 
 final class ComposerInspector
 {
+    private const DEFAULT_BIN_DIR = 'vendor/bin';
+
     /**
      * @var array<string,Package>
      */
     private array $packages = [];
 
+    private string $binDir = self::DEFAULT_BIN_DIR;
+
     private bool $loaded = false;
 
-    public function __construct(private string $path)
-    {
+    public function __construct(
+        private string $lockFile,
+        private string $composerFile,
+    ) {
     }
 
     public function package(string $name): ?Package
     {
-        $this->readFile();
+        $this->readFiles();
         if (!isset($this->packages[$name])) {
             return null;
         }
@@ -28,36 +34,46 @@ final class ComposerInspector
         return $this->packages[$name];
     }
 
-    private function readFile(): void
+    public function getBinDir(): string
     {
-        if ($this->loaded) {
-            return;
-        }
-        if (!file_exists($this->path)) {
+        $this->readFiles();
+        return $this->binDir;
+    }
+
+    private function readFiles(): void
+    {
+        if ($this->loaded || !file_exists($this->lockFile) || !file_exists($this->composerFile)) {
+            $this->loaded = true;
             return;
         }
 
-        $contents = file_get_contents($this->path);
+        $lockContent = $this->parseFile($this->lockFile);
+        foreach ($lockContent->{'packages'} ?? [] as $pkg) {
+            $this->packages[(string)$pkg->{'name'}] = $this->forVersion($pkg->{'name'}, $pkg->{'version'}, false);
+        }
+        foreach ($lockContent->{'packages-dev'} ?? [] as $pkg) {
+            $this->packages[(string)$pkg->{'name'}] = $this->forVersion($pkg->{'name'}, $pkg->{'version'}, true);
+        }
+
+        $composerContent = $this->parseFile($this->composerFile);
+        $this->binDir = $composerContent->{'bin-dir'} ?? self::DEFAULT_BIN_DIR;
+    }
+
+    private function parseFile(string $fileName): ?stdClass
+    {
+        $contents = file_get_contents($fileName);
 
         if (false === $contents) {
-            throw new RuntimeException(sprintf(
-                'Could not read file "%s"',
-                $this->path
-            ));
+            throw new RuntimeException(sprintf('Could not read file "%s"', $this->lockFile));
         }
 
         $obj = json_decode($contents);
 
         if (!$obj instanceof stdClass) {
-            return;
+            return null;
         }
 
-        foreach ($obj->{'packages'} ?? [] as $pkg) {
-            $this->packages[(string)$pkg->{'name'}] = $this->forVersion($pkg->{'name'}, $pkg->{'version'}, false);
-        }
-        foreach ($obj->{'packages-dev'} ?? [] as $pkg) {
-            $this->packages[(string)$pkg->{'name'}] = $this->forVersion($pkg->{'name'}, $pkg->{'version'}, true);
-        }
+        return $obj;
     }
 
     private function forVersion(string $name, string $version, bool $isDev): Package
