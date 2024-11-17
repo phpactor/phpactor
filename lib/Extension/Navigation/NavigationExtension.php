94dbc9a3ab8d81fd42b671dc0d20cff008abdecb
Bart Jaskulski <bjaskulski@protonmail.com>
2024-11-17 13:27:46 +0100

    Fix static analysis issues
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

--- lib/Extension/Navigation/NavigationExtension.php
+++ lib/Extension/Navigation/NavigationExtension.php
@@ -6,7 +6,6 @@ use Phpactor\Container\Container;
 use Phpactor\Container\ContainerBuilder;
 use Phpactor\Container\Extension;
 use Phpactor\Extension\FilePathResolver\FilePathResolverExtension;
-use Phpactor\Extension\LanguageServer\LanguageServerExtension;
 use Phpactor\Extension\Navigation\Application\Navigator;
 use Phpactor\Extension\Navigation\Handler\NavigateHandler;
 use Phpactor\Extension\Navigation\Navigator\ChainNavigator;
@@ -44,9 +43,13 @@ class NavigationExtension implements Extension
 
     private function registerPathFinder(ContainerBuilder $container): void
     {
-        $container->register(self::SERVICE_PATH_FINDER, function (Container $container) {
-            return PathFinder::fromAbsoluteDestinations($container->parameter(FilePathResolverExtension::PARAM_PROJECT_ROOT)->string(), $container->parameter(self::PATH_FINDER_DESTINATIONS)->value());
-        });
+        $container->register(
+            self::SERVICE_PATH_FINDER,
+            fn (Container $container) => PathFinder::fromAbsoluteDestinations(
+                $container->parameter(FilePathResolverExtension::PARAM_PROJECT_ROOT)->string(),
+                $container->parameter(self::PATH_FINDER_DESTINATIONS)->value() // @phpstan-ignore argument.type
+            )
+        );
 
         $container->register('application.navigator', function (Container $container) {
             return new Navigator(
