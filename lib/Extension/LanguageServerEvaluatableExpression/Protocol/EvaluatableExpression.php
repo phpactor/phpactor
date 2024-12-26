c255b204a5c7a2a25b1171a9d30cefc64749cc8f
Damjan Cvetko <damjan.cvetko@gmail.com>
2024-12-26 16:07:29 +0100

    Simplify EvaluatableExpression LSP object.

--- lib/Extension/LanguageServerEvaluatableExpression/Protocol/EvaluatableExpression.php
+++ lib/Extension/LanguageServerEvaluatableExpression/Protocol/EvaluatableExpression.php
@@ -1,9 +1,7 @@
-<?php // Auto-generated from vscode-languageserver-protocol (typescript)
+<?php
 
 namespace Phpactor\Extension\LanguageServerEvaluatableExpression\Protocol;
 
-use DTL\Invoke\Invoke;
-use Exception;
 use RuntimeException;
 use Phpactor\LanguageServerProtocol\Range;
 
@@ -25,72 +23,12 @@ class EvaluatableExpression
      */
     public static function fromArray(array $array, bool $allowUnknownKeys = false)
     {
-        $map = [
-            'expression' => ['names' => [], 'iterable' => false],
-            'range' => ['names' => [Range::class], 'iterable' => false],
-        ];
-
-        foreach ($array as $key => &$value) {
-            if (!isset($map[$key])) {
-                if ($allowUnknownKeys) {
-                    unset($array[$key]);
-                    continue;
-                }
-
-                throw new RuntimeException(sprintf(
-                    'Parameter "%s" on class "%s" not known, known parameters: "%s"',
-                    $key,
-                    self::class,
-                    implode('", "', array_keys($map))
-                ));
-            }
-
-            // from here we only care about arrays that can be transformed into
-            // objects
-            if (!is_array($value)) {
-                continue;
-            }
-
-            if (empty($map[$key]['names'])) {
-                continue;
-            }
-
-            if ($map[$key]['iterable']) {
-                $value = array_map(function ($object) use ($map, $key, $allowUnknownKeys) {
-                    if (!is_array($object)) {
-                        return $object;
-                    }
-
-                    return self::invokeFromNames($map[$key]['names'], $object, $allowUnknownKeys) ?: $object;
-                }, $value);
-                continue;
-            }
-
-            $names = $map[$key]['names'];
-            $value = self::invokeFromNames($names, $value, $allowUnknownKeys) ?: $value;
+        if (!is_array($array['range'])) {
+            throw new RuntimeException('Missing "range"');
         }
-
-        return Invoke::new(self::class, $array);
-    }
-
-    /**
-     * @param array<string> $classNames
-     * @param array<string,mixed> $object
-     */
-    private static function invokeFromNames(array $classNames, array $object, bool $allowUnknownKeys): ?object
-    {
-        $lastException = null;
-        foreach ($classNames as $className) {
-            try {
-                // @phpstan-ignore-next-line
-                return call_user_func_array($className . '::fromArray', [$object, $allowUnknownKeys]);
-            } catch (Exception $exception) {
-                $lastException = $exception;
-                continue;
-            }
-        }
-
-        throw $lastException;
+        $range = Range::fromArray($array['range']);
+        $expression = is_string($array['expression']) ? $array['expression'] : null;
+        return new self($range, $expression);
     }
 
 }
