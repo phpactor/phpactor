b11764bcbf66e71f05b5ca9b4df01625d6d607e8
przepompownia <przepompownia@users.noreply.github.com>
2025-01-03 04:15:42 +0100

    cs-fixer

--- lib/Completion/Bridge/TolerantParser/WorseReflection/KeywordCompletor.php
+++ lib/Completion/Bridge/TolerantParser/WorseReflection/KeywordCompletor.php
@@ -33,7 +33,6 @@ class KeywordCompletor implements TolerantCompletor
         '__unset' => "(string \\\$\${1:name}): void\n{\$0\n}",
         '__wakeup' => "(): void\n{\$0\n}",
     ];
-
     private const STATEMENTS = [
         'break' => '$1;$0',
         'continue' => '$1;$0',
