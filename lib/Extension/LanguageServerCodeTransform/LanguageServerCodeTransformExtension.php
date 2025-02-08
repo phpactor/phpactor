20bb12472d161bf55da09ace2beb2922ee86aff1
mamazu <14860264+mamazu@users.noreply.github.com>
2025-02-08 23:28:27 +0100

    Adding a way to prioritize code actions

--- lib/Extension/LanguageServerCodeTransform/LanguageServerCodeTransformExtension.php
+++ lib/Extension/LanguageServerCodeTransform/LanguageServerCodeTransformExtension.php
@@ -267,7 +267,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 $container->get(ReplaceQualifierWithImport::class)
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [],
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [ 'priority' => 100],
         ]);
 
         $container->register(ImportNameProvider::class, function (Container $container) {
@@ -276,7 +276,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 $container->getParameter(self::PARAM_REPORT_NON_EXISTING_NAMES)
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [],
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [ 'priority' => 100],
         ]);
         $container->register(OverridableMethodFinder::class, function (Container $container) {
             return new OverridableMethodFinder(
@@ -288,7 +288,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 $container->get(OverridableMethodFinder::class),
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [],
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => ['priority' => -200],
         ]);
 
         $container->register(TransformerCodeActionPovider::class.'promote_constructor_private', function (Container $container) {
@@ -308,7 +308,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 'Promote Constructor (public)'
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => []
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [ 'priority' => 100]
         ]);
 
         $container->register(TransformerCodeActionPovider::class.'complete_constructor_private', function (Container $container) {
@@ -318,7 +318,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 'Complete Constructor (private)'
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => []
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [ 'priority' => 100]
         ]);
 
         $container->register(TransformerCodeActionPovider::class.'complete_constructor_public', function (Container $container) {
@@ -328,7 +328,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 'Complete Constructor (public)'
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => []
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [ 'priority' => 100]
         ]);
         $container->register(TransformerCodeActionPovider::class.'add_missing_class_generic', function (Container $container) {
             return new TransformerCodeActionPovider(
@@ -435,7 +435,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 'Remove unused imports'
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => []
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [ 'priority' => 100]
         ]);
 
         $container->register(GenerateMemberProvider::class, function (Container $container) {
@@ -499,7 +499,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 'fill new object construct with named parameters',
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => []
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [ 'priority' => 100]
         ]);
         $container->register(ByteOffsetRefactorProvider::class.'.fill_match_arms', function (Container $container) {
             return new ByteOffsetRefactorProvider(
@@ -519,7 +519,7 @@ class LanguageServerCodeTransformExtension implements Extension
                 'replace string with HereDoc',
             );
         }, [
-            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => []
+            LanguageServerExtension::TAG_CODE_ACTION_PROVIDER => [ 'priority' => 100]
         ]);
 
         $container->register(GenerateConstructorProvider::class, function (Container $container) {
