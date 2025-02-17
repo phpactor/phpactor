b9172c11a578ba3e98429e8845faa3e0261ed0c7
Jiri Velek <bleksak@protonmail.com>
2025-02-17 13:38:43 +0100

    feat: add symfony form type options completion
    Completions for FormType options using the FormTypeBuilder

--- lib/Extension/Symfony/Completor/SymfonyContainerCompletor.php
+++ lib/Extension/Symfony/Completor/SymfonyContainerCompletor.php
@@ -4,23 +4,38 @@ namespace Phpactor\Extension\Symfony\Completor;
 
 use Generator;
 use Microsoft\PhpParser\Node;
+use Microsoft\PhpParser\Node\ArrayElement;
+use Microsoft\PhpParser\Node\DelimitedList\ArgumentExpressionList;
+use Microsoft\PhpParser\Node\DelimitedList\ArrayElementList;
+use Microsoft\PhpParser\Node\Expression\ArgumentExpression;
+use Microsoft\PhpParser\Node\Expression\ArrayCreationExpression;
+use Microsoft\PhpParser\Node\Expression\BracedExpression;
 use Microsoft\PhpParser\Node\Expression\CallExpression;
+use Microsoft\PhpParser\Node\Expression\ListIntrinsicExpression;
 use Microsoft\PhpParser\Node\Expression\MemberAccessExpression;
+use Microsoft\PhpParser\Node\Expression\ParenthesizedExpression;
 use Microsoft\PhpParser\Node\QualifiedName;
 use Microsoft\PhpParser\Node\StringLiteral;
 use Phpactor\Completion\Bridge\TolerantParser\TolerantCompletor;
 use Phpactor\Completion\Core\Suggestion;
+// use Phpactor\Extension\Symfony\Model\SymfonyCommandRunner;
+use Phpactor\Extension\Symfony\Model\FormTypeCompletionCache;
+use Phpactor\Extension\Symfony\Model\SymfonyCommandRunner;
 use Phpactor\Extension\Symfony\Model\SymfonyContainerInspector;
 use Phpactor\Extension\Symfony\Model\SymfonyContainerService;
 use Phpactor\TextDocument\ByteOffset;
 use Phpactor\TextDocument\TextDocument;
+use Phpactor\WorseReflection\Core\ClassName;
 use Phpactor\WorseReflection\Core\TypeFactory;
+use Phpactor\WorseReflection\Core\Type\ClassStringType;
 use Phpactor\WorseReflection\Core\Util\NodeUtil;
 use Phpactor\WorseReflection\Reflector;
+use classObj;
 
 class SymfonyContainerCompletor implements TolerantCompletor
 {
     const CONTAINER_CLASS = 'Symfony\\Component\\DependencyInjection\\ContainerInterface';
+    const FORM_BUILDER_INTERFACE = 'Symfony\\Component\\Form\\FormBuilderInterface';
 
     public function __construct(private Reflector $reflector, private SymfonyContainerInspector $inspector)
     {
@@ -28,6 +43,8 @@ class SymfonyContainerCompletor implements TolerantCompletor
 
     public function complete(Node $node, TextDocument $source, ByteOffset $offset): Generator
     {
+        yield from $this->completeFormTypeOptions($node, $source, $offset);
+
         $inQuote = false;
         if ($node instanceof StringLiteral && $node->parent->parent) {
             $inQuote = true;
@@ -88,6 +105,101 @@ class SymfonyContainerCompletor implements TolerantCompletor
                 'priority' => 555,
             ]);
         }
+
+        return true;
+    }
+
+    private function completeFormTypeOptions(Node $node, TextDocument $source, ByteOffset $offset): Generator
+    {
+        if(!($node instanceof StringLiteral)) {
+            return;
+        }
+
+        if(!($node->parent instanceof ArrayElement)) {
+            return;
+        }
+
+        $arrayElementNode = $node->parent;
+
+        if (!($arrayElementNode->parent instanceof ArrayElementList)) {
+            return;
+        }
+
+        $arrayElementList = $arrayElementNode->parent;
+
+        $callNode = $node->getFirstAncestor(CallExpression::class);
+
+        $argumentListNode = $callNode->getFirstDescendantNode(ArgumentExpressionList::class);
+
+        $memberAccess = $callNode->callableExpression;
+
+        if (!$memberAccess instanceof MemberAccessExpression) {
+            return;
+        }
+
+        $methodName = NodeUtil::nameFromTokenOrNode($callNode, $memberAccess->memberName);
+
+        if ($methodName !== 'add') {
+            return;
+        }
+
+        $expression = $memberAccess->dereferencableExpression;
+        $containerType = $this->reflector->reflectOffset($source, $expression->getEndPosition())->nodeContext()->type();
+
+        if ($containerType->instanceof(TypeFactory::class(self::FORM_BUILDER_INTERFACE))->isFalseOrMaybe()) {
+            return;
+        }
+
+        // get second argument
+
+        $generator = $argumentListNode->getChildNodes();
+        $generator->next();
+
+        $formTypeNode = $generator->current();
+
+        if(!($formTypeNode instanceof ArgumentExpression)) {
+            return;
+        }
+
+        $formTypeClassType = $this->reflector->reflectOffset($source, $formTypeNode->getEndPosition())->nodeContext()->type();
+
+        if(!($formTypeClassType instanceof ClassStringType)) {
+            return;
+        }
+
+        $formTypeClassFQN = $formTypeClassType->className()->full();
+
+        yield from FormTypeCompletionCache::complete($formTypeClassFQN);
+
+        // $result = SymfonyCommandRunner::run('debug:form --format json', $formTypeClassFQN);
+
+        // $data = json_decode($result, true);
+
+        // $options = $data['options'] ?? [];
+        // $ownOptions = $options['own'] ?? [];
+        // $parentOptions = $options['parent'] ?? [];
+
+        // foreach ($ownOptions as $option) {
+        //     yield Suggestion::createWithOptions($option, [
+        //         'label' => $option,
+        //         'short_description' => $option,
+        //         'documentation' => $option,
+        //         'type' => Suggestion::TYPE_CONSTANT,
+        //         'priority' => 555,
+        //     ]);
+        // }
+
+        // foreach($parentOptions as $parentType => $options) {
+        //     foreach ($options as $option) {
+        //         yield Suggestion::createWithOptions($option, [
+        //             'label' => $option,
+        //             'short_description' => $parentType,
+        //             'documentation' => $parentType,
+        //             'type' => Suggestion::TYPE_CONSTANT,
+        //             'priority' => 555,
+        //         ]);
+        //     }
+        // }
 
         return true;
     }
---
+++ lib/Extension/Symfony/Model/FormTypeCompletion.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Phpactor\Extension\Symfony\Model;
+
+use Generator;
+use Phpactor\Completion\Core\Suggestion;
+
+final readonly class FormTypeCompletion
+{
+    /**
+     * @param array<string> $options
+     */
+    public function __construct(
+        public ?string $parentClass,
+        public array $options = []
+    )
+    {
+    }
+
+    public function getCompletions(): Generator
+    {
+        foreach($this->options as $option) {
+            yield Suggestion::createWithOptions($option, [
+                'label' => $option,
+                'short_description' => '',
+                'documentation' => '',
+                'type' => Suggestion::TYPE_CONSTANT,
+                'priority' => 555,
+            ]);
+        }
+
+        return true;
+    }
+}
