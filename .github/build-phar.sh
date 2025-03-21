7838fdb5776aed3a25bedd1aad0817be5f8aafb7
Damjan Cvetko <damjan.cvetko@gmail.com>
2025-03-21 01:02:55 +0100

    Box 4.3.8 does not work on windows. Upgrading to latest.

--- .github/build-phar.sh
+++ .github/build-phar.sh
@@ -6,7 +6,7 @@ composer install --no-dev
 mkdir -p build
 cd build
 
-wget -O box.phar https://github.com/box-project/box/releases/download/4.3.8/box.phar
+wget -O box.phar https://github.com/box-project/box/releases/download/4.6.6/box.phar
 php box.phar compile -c ../box.json
 
 cd -
