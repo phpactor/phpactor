080e1a8f3f49ef5e2f2cf51277b4624bee6f4ef7
przepompownia <przepompownia@users.noreply.github.com>
2025-10-09 16:00:08 +0200

    Refactor and complete IndexedNameSearcherTest

--- lib/Indexer/Tests/Adapter/ReferenceFinder/IndexedNameSearcherTest.php
+++ lib/Indexer/Tests/Adapter/ReferenceFinder/IndexedNameSearcherTest.php
@@ -2,6 +2,7 @@
 
 namespace Phpactor\Indexer\Tests\Adapter\ReferenceFinder;
 
+use Generator;
 use Phpactor\Indexer\Adapter\ReferenceFinder\IndexedNameSearcher;
 use Phpactor\Indexer\Tests\Adapter\IndexTestCase;
 use Phpactor\ReferenceFinder\NameSearcherType;
@@ -9,6 +10,18 @@ use Phpactor\ReferenceFinder\Search\NameSearchResult;
 
 class IndexedNameSearcherTest extends IndexTestCase
 {
+    private const ATTR_WORKSPACE = [
+        'project/Baj.php' => '<?php #[Attribute(\Attribute::TARGET_CLASS)] class Baj {}',
+        'project/Bacc.php' => '<?php #[Attribute(\Attribute::TARGET_CLASS_CONSTANT)] class Bacc {}',
+        'project/Baf.php' => '<?php #[Attribute(\Attribute::TARGET_FUNCTION)] class Baf {}',
+        'project/Bam.php' => '<?php #[Attribute] class Bam {}',
+        'project/Bap.php' => '<?php use Attribute as PHPAttribute; #[PHPAttribute(\Attribute::TARGET_PARAMETER)] class Bap {}',
+        'project/Bar.php' => '<?php #[\Attribute(\Attribute::TARGET_PROPERTY)] class Bar {}',
+        'project/Foo/Bax.php' => '<?php namespace Foo; use Not\Attribute; #[Attribute] class Bax {}',
+        'project/Baz.php' => '<?php class Baz {}',
+        'project/Attribute/Bak.php' => '<?php namespace Attribute; #[\Attribute(\Attribute::TARGET_METHOD)] readonly class Bak {}',
+    ];
+
     public function testSearcherWithAbsolute(): void
     {
         $this->workspace()->put('project/Foobar.php', '<?php class Foobar {}');
@@ -33,6 +46,7 @@ class IndexedNameSearcherTest extends IndexTestCase
         foreach ($searcher->search('Foo') as $result) {
             assert($result instanceof NameSearchResult);
             self::assertEquals('Foobar', $result->name()->head()->__toString());
+            self::assertNotNull($result->uri());
             self::assertStringContainsString('Foobar.php', $result->uri()->__toString());
         }
     }
@@ -47,6 +61,7 @@ class IndexedNameSearcherTest extends IndexTestCase
         foreach ($searcher->search('Foo', NameSearcherType::INTERFACE) as $result) {
             assert($result instanceof NameSearchResult);
             self::assertEquals('Foobar', $result->name()->head()->__toString());
+            self::assertNotNull($result->uri());
             self::assertStringContainsString('Foobar.php', $result->uri()->__toString());
             return;
         }
@@ -64,6 +79,7 @@ class IndexedNameSearcherTest extends IndexTestCase
         foreach ($searcher->search('Foo', NameSearcherType::ENUM) as $result) {
             assert($result instanceof NameSearchResult);
             self::assertEquals('Foobar', $result->name()->head()->__toString());
+            self::assertNotNull($result->uri());
             self::assertStringContainsString('Foobar.php', $result->uri()->__toString());
             return;
         }
@@ -81,6 +97,7 @@ class IndexedNameSearcherTest extends IndexTestCase
         foreach ($searcher->search('Foo', NameSearcherType::TRAIT) as $result) {
             assert($result instanceof NameSearchResult);
             self::assertEquals('Foobar', $result->name()->head()->__toString());
+            self::assertNotNull($result->uri());
             self::assertStringContainsString('Foobar.php', $result->uri()->__toString());
             return;
         }
@@ -88,27 +105,113 @@ class IndexedNameSearcherTest extends IndexTestCase
         $this->fail('Could not find trait');
     }
 
-    public function testSearcherForAttribute(): void
+    /**
+     * @dataProvider provideWorkspaceToSearchAttributes
+     * @param NameSearcherType::* $type
+     * @param string[] $expectedResultPaths
+     */
+    public function testSearcherForAttribute(string $query, string $type, array $expectedResultPaths): void
     {
-        $this->workspace()->put('project/Bap.php', '<?php use Attribute as PHPAttribute; #[PHPAttribute] class Bap {}');
-        $this->workspace()->put('project/Bar.php', '<?php #[\Attribute] class Bar {}');
-        $this->workspace()->put('project/Baj.php', '<?php #[Attribute] class Baj {}');
-        $this->workspace()->put('project/Foo/Bax.php', '<?php namespace Foo; use Not\Attribute; #[Attribute] class Bax {}');
-        $this->workspace()->put('project/Baz.php', '<?php class Baz {}');
-        $this->workspace()->put('project/Attribute/Bak.php', '<?php namespace Attribute; #[\Attribute] readonly class Bak {}');
+        foreach (self::ATTR_WORKSPACE as $path => $contents) {
+            $this->workspace()->put($path, $contents);
+        }
+
         $agent = $this->indexAgent();
         $agent->indexer()->getJob()->run();
         $searcher = new IndexedNameSearcher($agent->search());
 
-        $count = 0;
-
-        foreach ($searcher->search('Ba', NameSearcherType::ATTRIBUTE) as $result) {
+        $resultPaths = [];
+        $offset = 1 + mb_strlen($this->workspace()->path());
+        foreach ($searcher->search($query, $type) as $result) {
             assert($result instanceof NameSearchResult);
-            self::assertContainsEquals($result->name()->head()->__toString(), ['Bar', 'Baj', 'Bap', 'Bak']);
-            self::assertMatchesRegularExpression('#project(/Attribute)?/Ba[rjpk].php$#', $result->uri()->__toString());
-            ++$count;
+            self::assertNotNull($result->uri());
+            $resultPaths[] = mb_substr($result->uri()->path(), $offset);
         }
 
-        self::assertSame(4, $count);
+        self::assertEqualsCanonicalizing($expectedResultPaths, $resultPaths);
+    }
+
+    /**
+     * @return Generator<string,array{query:string,type:string,expectedResultPaths:array<int,string>}>
+     */
+    public function provideWorkspaceToSearchAttributes(): Generator
+    {
+        yield 'not targeted attributes' => [
+            'query' => 'Ba',
+            'type' => NameSearcherType::ATTRIBUTE,
+            'expectedResultPaths' => [
+                'project/Baf.php',
+                'project/Bacc.php',
+                'project/Bap.php',
+                'project/Bar.php',
+                'project/Baj.php',
+                'project/Bam.php',
+                'project/Attribute/Bak.php',
+            ],
+        ];
+
+        yield 'class attributes' => [
+            'query' => 'Ba',
+            'type' => NameSearcherType::ATTRIBUTE_TARGET_CLASS,
+            'expectedResultPaths' => [
+                'project/Baj.php',
+                'project/Bam.php',
+            ],
+        ];
+
+        yield 'property attributes' => [
+            'query' => 'Ba',
+            'type' => NameSearcherType::ATTRIBUTE_TARGET_PROPERTY,
+            'expectedResultPaths' => [
+                'project/Bar.php',
+                'project/Bam.php',
+            ],
+        ];
+
+        yield 'promoted property attributes' => [
+            'query' => 'Ba',
+            'type' => NameSearcherType::ATTRIBUTE_TARGET_PROMOTED_PROPERTY,
+            'expectedResultPaths' => [
+                'project/Bar.php',
+                'project/Bap.php',
+                'project/Bam.php',
+            ],
+        ];
+
+        yield 'method attributes' => [
+            'query' => 'Ba',
+            'type' => NameSearcherType::ATTRIBUTE_TARGET_METHOD,
+            'expectedResultPaths' => [
+                'project/Attribute/Bak.php',
+                'project/Bam.php',
+            ],
+        ];
+
+        yield 'parameter attributes' => [
+            'query' => 'Ba',
+            'type' => NameSearcherType::ATTRIBUTE_TARGET_PARAMETER,
+            'expectedResultPaths' => [
+                'project/Bap.php',
+                'project/Bam.php',
+            ],
+        ];
+
+        yield 'class constant attributes' => [
+            'query' => 'Ba',
+            'type' => NameSearcherType::ATTRIBUTE_TARGET_CLASS_CONSTANT,
+            'expectedResultPaths' => [
+                'project/Bacc.php',
+                'project/Bam.php',
+            ],
+        ];
+
+        yield 'function attributes' => [
+            'query' => 'Ba',
+            'type' => NameSearcherType::ATTRIBUTE_TARGET_FUNCTION,
+            'expectedResultPaths' => [
+                'project/Baf.php',
+                'project/Bam.php',
+            ],
+        ];
     }
 }
