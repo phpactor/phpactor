4143c4eacbb6a598e33ff9b730194a276f3c9e35
przepompownia <przepompownia@users.noreply.github.com>
2025-11-11 21:36:21 +0100

    Update docs

--- lib/Extension/LanguageServerPhpCsFixer/LanguageServerPhpCsFixerExtension.php
+++ lib/Extension/LanguageServerPhpCsFixer/LanguageServerPhpCsFixerExtension.php
@@ -97,7 +97,7 @@ class LanguageServerPhpCsFixerExtension implements OptionalExtension
 
         $schema->setDescriptions([
             self::PARAM_PHP_CS_FIXER_BIN => 'Path to the php-cs-fixer executable',
-            self::PARAM_PHP_CS_FIXER_VERSION => 'Arbitrary php-cs-fixer version (to avoid autodetection)',
+            self::PARAM_PHP_CS_FIXER_VERSION => 'Arbitrary php-cs-fixer version (to avoid autodetection, usable only to ignore PHP version)',
             self::PARAM_ENV => 'Environment for PHP CS Fixer',
             self::PARAM_SHOW_DIAGNOSTICS => 'Whether PHP CS Fixer diagnostics are shown',
             self::PARAM_CONFIG => 'Set custom PHP CS config path. Ex., %project_root%/.php-cs-fixer.php',
